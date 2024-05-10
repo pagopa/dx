4ee8133680b0f45d7ddcded8c7fe6d33e0cc2353
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-05-10 18:53:23 +0200

    lint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,9 +1,9 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-plugin-prettier/recommended";
 import comments from "@eslint-community/eslint-plugin-eslint-comments";
-import vitest from "eslint-plugin-vitest";
 import perfectionistNatural from "eslint-plugin-perfectionist/configs/recommended-natural";
+import prettier from "eslint-plugin-prettier/recommended";
+import vitest from "eslint-plugin-vitest";
+import tseslint from "typescript-eslint";
 
 export default [
   eslint.configs.recommended,
@@ -26,22 +26,22 @@ export default [
   },
   {
     rules: {
-      "max-lines-per-function": ["error", 200],
+      "@typescript-eslint/no-unused-expressions": ["error"],
+      "@typescript-eslint/no-unused-vars": ["error", { args: "after-used" }],
       "arrow-body-style": "error",
       complexity: "error",
-      "guard-for-in": "error",
       eqeqeq: ["error", "smart"],
+      "guard-for-in": "error",
+      "max-lines-per-function": ["error", 200],
       "no-bitwise": "error",
-      "no-eval": "error",
       "no-console": "error",
+      "no-eval": "error",
       "no-new-wrappers": "error",
-      "no-undef-init": "error",
       "no-param-reassign": "error",
+      "no-undef-init": "error",
       "no-var": "error",
       "prefer-const": "error",
       radix: "error",
-      "@typescript-eslint/no-unused-expressions": ["error"],
-      "@typescript-eslint/no-unused-vars": ["error", { args: "after-used" }],
     },
   },
 ];
