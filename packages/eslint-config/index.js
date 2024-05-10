06b049a027aad6a40fd24840a5c3a945eb3a2235
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-05-10 18:52:50 +0200

    switch to eslint 8

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,26 +1,47 @@
-import js from "@eslint/js";
-import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
-import prettier from "eslint-config-prettier";
-import perfectionist from "eslint-plugin-perfectionist/configs/recommended-natural";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
+import comments from "@eslint-community/eslint-plugin-eslint-comments";
 import vitest from "eslint-plugin-vitest";
-import ts from "typescript-eslint";
+import perfectionistNatural from "eslint-plugin-perfectionist/configs/recommended-natural";
 
 export default [
-  js.configs.recommended,
-  ...ts.configs.strict,
-  ...ts.configs.stylistic,
-  prettier,
-  perfectionist,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   comments.configs.recommended,
+  prettier,
+  perfectionistNatural,
   {
-    ...vitest.configs.recommended,
+    files: ["tests/**", "__tests__/**"],
     rules: {
-      ...vitest.configs.recommended.rules,
+      ...vitest.configs.recommended,
       "prefer-called-with": ["error"],
       "prefer-equality-matcher": ["error"],
       "prefer-expect-resolves": ["error"],
       "prefer-spy-on": ["error"],
       "prefer-todo": ["error"],
+    },
+  },
+  {
+    rules: {
+      "max-lines-per-function": ["error", 200],
+      "arrow-body-style": "error",
+      complexity: "error",
+      "guard-for-in": "error",
+      eqeqeq: ["error", "smart"],
+      "no-bitwise": "error",
+      "no-eval": "error",
+      "no-console": "error",
+      "no-new-wrappers": "error",
+      "no-undef-init": "error",
+      "no-param-reassign": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      radix: "error",
+      "@typescript-eslint/no-unused-expressions": ["error"],
+      "@typescript-eslint/no-unused-vars": ["error", { args: "after-used" }],
     },
   },
 ];
