a8bf0f15888797f567af25fa6af063d4e9ee7f35
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-14 16:49:38 +0200

    fix roles

--- infra/modules/azure_function_app/function_app.tf
+++ infra/modules/azure_function_app/function_app.tf
@@ -143,35 +143,35 @@ resource "azurerm_linux_function_app_slot" "this" {
 resource "azurerm_role_assignment" "function_storage_blob_data_owner" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_linux_function_app.this.id
+  principal_id         = azurerm_linux_function_app.this.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "staging_function_storage_blob_data_owner" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_linux_function_app_slot.this.id
+  principal_id         = azurerm_linux_function_app_slot.this.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "function_storage_account_contributor" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Account Contributor"
-  principal_id         = azurerm_linux_function_app.this.id
+  principal_id         = azurerm_linux_function_app.this.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "staging_function_storage_account_contributor" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Account Contributor"
-  principal_id         = azurerm_linux_function_app_slot.this.id
+  principal_id         = azurerm_linux_function_app_slot.this.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "function_storage_queue_data_contributor" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Queue Data Contributor"
-  principal_id         = azurerm_linux_function_app.this.id
+  principal_id         = azurerm_linux_function_app.this.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "staging_function_storage_queue_data_contributor" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Queue Data Contributor"
-  principal_id         = azurerm_linux_function_app_slot.this.id
+  principal_id         = azurerm_linux_function_app_slot.this.identity[0].principal_id
 }
