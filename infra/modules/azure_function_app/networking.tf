aa78bff67dfa082ab4db995737e14f44dc895ec8
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-17 17:40:31 +0200

    remove file pep

--- infra/modules/azure_function_app/networking.tf
+++ infra/modules/azure_function_app/networking.tf
@@ -19,48 +19,6 @@ resource "azurerm_private_endpoint" "st_blob" {
   tags = var.tags
 }
 
-resource "azurerm_private_endpoint" "st_file" {
-  name                = "${local.project}-${var.domain}-${var.app_name}-file-pep-${var.instance_number}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_pep_id
-
-  private_service_connection {
-    name                           = "${local.project}-${var.domain}-${var.app_name}-file-pep-${var.instance_number}"
-    private_connection_resource_id = azurerm_storage_account.this.id
-    is_manual_connection           = false
-    subresource_names              = ["file"]
-  }
-
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.storage_account_file.id]
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_private_endpoint" "st_table" {
-  name                = "${local.project}-${var.domain}-${var.app_name}-table-pep-${var.instance_number}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_pep_id
-
-  private_service_connection {
-    name                           = "${local.project}-${var.domain}-${var.app_name}-table-pep-${var.instance_number}"
-    private_connection_resource_id = azurerm_storage_account.this.id
-    is_manual_connection           = false
-    subresource_names              = ["table"]
-  }
-
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.storage_account_table.id]
-  }
-
-  tags = var.tags
-}
-
 resource "azurerm_private_endpoint" "st_queue" {
   name                = "${local.project}-${var.domain}-${var.app_name}-queue-pep-${var.instance_number}"
   location            = var.location
--- infra/modules/azure_function_app/subnets.tf
+++ infra/modules/azure_function_app/subnets.tf
@@ -1,9 +1,8 @@
 resource "azurerm_subnet" "this" {
-  name                              = "${local.project}-${var.domain}-${var.app_name}-snet-${var.instance_number}"
-  virtual_network_name              = data.azurerm_virtual_network.this.name
-  resource_group_name               = data.azurerm_virtual_network.this.resource_group_name
-  address_prefixes                  = [var.subnet_cidr]
-  private_endpoint_network_policies = "Disabled"
+  name                 = "${local.project}-${var.domain}-${var.app_name}-snet-${var.instance_number}"
+  virtual_network_name = data.azurerm_virtual_network.this.name
+  resource_group_name  = data.azurerm_virtual_network.this.resource_group_name
+  address_prefixes     = [var.subnet_cidr]
 
   delegation {
     name = "default"
