1724b3c430705c39a24a3a315573e6c64b62d507
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-27 11:29:52 +0200

    add appservice

---
+++ infra/modules/azure_app_service/networking.tf
@@ -0,0 +1,43 @@
+resource "azurerm_private_endpoint" "app_service_sites" {
+  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-app-pep-${var.environment.instance_number}"
+  location            = var.environment.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_pep_id
+
+  private_service_connection {
+    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-app-pep-${var.environment.instance_number}"
+    private_connection_resource_id = azurerm_linux_web_app.this.id
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.app_service.id]
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_private_endpoint" "staging_app_service_sites" {
+  count = local.app_service.is_slot_enabled
+
+  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-staging-app-pep-${var.environment.instance_number}"
+  location            = var.environment.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_pep_id
+
+  private_service_connection {
+    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-staging-app-pep-${var.environment.instance_number}"
+    private_connection_resource_id = azurerm_linux_web_app.this.id
+    is_manual_connection           = false
+    subresource_names              = ["sites-${azurerm_linux_web_app_slot.this[0].name}"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.app_service.id]
+  }
+
+  tags = var.tags
+}
