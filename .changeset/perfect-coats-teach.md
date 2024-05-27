8c68693c58716136c48f2564a5b919e648454824
Luca Cavallaro <luca.cavallaro@pagopa.it>
2024-05-27 10:34:50 +0200

    [DEVEX-101] Add @pagopa/eslint-config

---
+++ .changeset/perfect-coats-teach.md
@@ -0,0 +1,5 @@
+---
+"@pagopa/eslint-config": major
+---
+
+Updated configuration to match our current code style, switched to flat config file.
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,45 @@
+import eslint from "@eslint/js";
+import perfectionistNatural from "eslint-plugin-perfectionist/configs/recommended-natural";
+import prettier from "eslint-plugin-prettier/recommended";
+import vitest from "eslint-plugin-vitest";
+import tseslint from "typescript-eslint";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  prettier,
+  perfectionistNatural,
+  {
+    files: ["tests/**", "__tests__/**"],
+    rules: {
+      ...vitest.configs.recommended,
+      "prefer-called-with": ["error"],
+      "prefer-equality-matcher": ["error"],
+      "prefer-expect-resolves": ["error"],
+      "prefer-spy-on": ["error"],
+      "prefer-todo": ["error"],
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": ["error"],
+      "@typescript-eslint/no-unused-vars": ["error", { args: "after-used" }],
+      "arrow-body-style": "error",
+      complexity: "error",
+      eqeqeq: ["error", "smart"],
+      "guard-for-in": "error",
+      "max-lines-per-function": ["error", 200],
+      "no-bitwise": "error",
+      "no-console": "error",
+      "no-eval": "error",
+      "no-new-wrappers": "error",
+      "no-param-reassign": "error",
+      "no-undef-init": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      radix: "error",
+    },
+  },
+];
