a8a182ed7a7c624452036b43ac8ca22b18c26938
LAP-MBP16-N02-0359 <lap-mbp16-n02-0359@MBPdiLA6N020359.homenet.telecomitalia.it>
2024-06-12 11:55:38 +0200

    added drift detection test and test resource creation azure

--- infra/resources/prod/westeurope/main.tf
+++ infra/resources/prod/westeurope/main.tf
@@ -18,3 +18,11 @@ provider "azurerm" {
   features {
   }
 }
+
+resource "azurerm_storage_account" "main" {
+  name                     = "teststoragemario1"
+  resource_group_name      = "dev-mario"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
