71430bdfb191cbb1badba1cca02839f14208de72
Mario Mupo <mario.mupo@pagopa.it>
2024-06-12 18:15:12 +0200

    edit github repository

--- infra/repository/github_repository.tf
+++ infra/repository/github_repository.tf
@@ -1,27 +1,35 @@
 resource "github_repository" "this" {
   name        = "dx"
-  description = "DevEx repository for shared tools and pipelines."
+  description = "Devex repository for shared tools and pipelines."
 
   visibility = "public"
 
-  allow_auto_merge            = true
+  allow_auto_merge            = false
   allow_rebase_merge          = false
   allow_merge_commit          = false
   allow_squash_merge          = true
   squash_merge_commit_title   = "PR_TITLE"
   squash_merge_commit_message = "BLANK"
 
-  delete_branch_on_merge = true
+  delete_branch_on_merge = false
 
   has_projects    = false
-  has_wiki        = false
-  has_discussions = false
+
   has_issues      = false
   has_downloads   = false
 
-  topics = ["dx"]
-
   vulnerability_alerts = true
 
-  archive_on_destroy = true
+  pages {
+    build_type = "workflow"
+    custom_404 = false
+    html_url   = "https://pagopa.github.io/dx/"
+    url        = "https://api.github.com/repos/pagopa/dx/pages"
+
+    source {
+        branch = "main"
+        path   = "/"
+      }
+    }
+
 }
