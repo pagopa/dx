a5aff5ee3b6e366fff6d87c9c97658e0357ed961
Mario Mupo <mario.mupo@pagopa.it>
2024-06-18 16:12:47 +0200

    changed dev to prod in repository

--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -1,9 +1,9 @@
-data "azurerm_user_assigned_identity" "identity_dev_ci" {
+data "azurerm_user_assigned_identity" "identity_prod_ci" {
   name                = "${local.project}-github-ci-identity"
   resource_group_name = local.identity_resource_group_name
 }
 
-data "azurerm_user_assigned_identity" "identity_dev_cd" {
+data "azurerm_user_assigned_identity" "identity_prod_cd" {
   name                = "${local.project}-github-cd-identity"
   resource_group_name = local.identity_resource_group_name
 }
--- infra/repository/github_environment_cd.tf
+++ infra/repository/github_environment_cd.tf
@@ -1,5 +1,5 @@
-resource "github_repository_environment" "github_repository_environment_dev_cd" {
-  environment = "dev-cd"
+resource "github_repository_environment" "github_repository_environment_prod_cd" {
+  environment = "prod-cd"
   repository  = github_repository.this.name
 
   deployment_branch_policy {
@@ -8,11 +8,11 @@ resource "github_repository_environment" "github_repository_environment_dev_cd" 
   }
 }
 
-resource "github_actions_environment_secret" "env_dev_cd_secrets" {
+resource "github_actions_environment_secret" "env_prod_cd_secrets" {
   for_each = local.cd.secrets
 
   repository      = github_repository.this.name
-  environment     = github_repository_environment.github_repository_environment_dev_cd.environment
+  environment     = github_repository_environment.github_repository_environment_prod_cd.environment
   secret_name     = each.key
   plaintext_value = each.value
 }
