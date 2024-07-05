4b8af7e16e2567b4d9492043a15068cf024872f5
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-05 12:21:55 +0200

    fix: not creating event_hub data when the name is *

--- infra/modules/azure_role_assignments/modules/event_hub/data.tf
+++ infra/modules/azure_role_assignments/modules/event_hub/data.tf
@@ -5,7 +5,7 @@ data "azurerm_eventhub_namespace" "this" {
 }
 
 data "azurerm_eventhub" "this" {
-  for_each            = { for event_hub in local.event_hubs : "${event_hub.namespace_name}|${event_hub.event_hub_name}" => event_hub }
+  for_each            = { for event_hub in local.event_hubs : "${event_hub.namespace_name}|${event_hub.event_hub_name}" => event_hub if event_hub.event_hub_name != "*" }
   name                = each.value.event_hub_name
   namespace_name      = each.value.namespace_name
   resource_group_name = each.value.resource_group_name
