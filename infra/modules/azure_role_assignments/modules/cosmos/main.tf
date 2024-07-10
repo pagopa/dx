3663dec0e22b06db0dfb4956b2dbfc181597b827
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-10 11:46:23 +0200

    fix: removed wildcards in cosmosdb scope

--- infra/modules/azure_role_assignments/modules/cosmos/main.tf
+++ infra/modules/azure_role_assignments/modules/cosmos/main.tf
@@ -5,5 +5,5 @@ resource "azurerm_cosmosdb_sql_role_assignment" "this" {
   account_name        = each.value.account_name
   role_definition_id  = "${data.azurerm_cosmosdb_account.cosmos["${each.value.resource_group_name}|${each.value.account_name}"].id}/sqlRoleDefinitions/${local.role_definition_id[lower(each.value.role)]}"
   principal_id        = var.principal_id
-  scope               = "${data.azurerm_cosmosdb_account.cosmos["${each.value.resource_group_name}|${each.value.account_name}"].id}/dbs/${each.value.database}/colls/${each.value.collection}"
+  scope               = "${data.azurerm_cosmosdb_account.cosmos["${each.value.resource_group_name}|${each.value.account_name}"].id}${each.value.scope}"
 }
