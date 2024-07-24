6df502f3b80e3658e3a08138f7f8438446ea3f94
Andrea Grillo <andrea.grillo@pagopa.it>
2024-07-24 18:18:28 +0200

    name validation

--- infra/modules/azure_function_app/networking.tf
+++ infra/modules/azure_function_app/networking.tf
@@ -1,11 +1,11 @@
 resource "azurerm_private_endpoint" "st_blob" {
-  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-blob-pep-${var.environment.instance_number}"
+  name                = local.storage_account.pep_blob_name
   location            = var.environment.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_pep_id
 
   private_service_connection {
-    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-blob-pep-${var.environment.instance_number}"
+    name                           = local.storage_account.pep_blob_name
     private_connection_resource_id = azurerm_storage_account.this.id
     is_manual_connection           = false
     subresource_names              = ["blob"]
@@ -20,13 +20,13 @@ resource "azurerm_private_endpoint" "st_blob" {
 }
 
 resource "azurerm_private_endpoint" "st_file" {
-  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-file-pep-${var.environment.instance_number}"
+  name                = local.storage_account.pep_file_name
   location            = var.environment.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_pep_id
 
   private_service_connection {
-    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-file-pep-${var.environment.instance_number}"
+    name                           = local.storage_account.pep_file_name
     private_connection_resource_id = azurerm_storage_account.this.id
     is_manual_connection           = false
     subresource_names              = ["file"]
@@ -41,13 +41,13 @@ resource "azurerm_private_endpoint" "st_file" {
 }
 
 resource "azurerm_private_endpoint" "st_queue" {
-  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-queue-pep-${var.environment.instance_number}"
+  name                = local.storage_account.pep_queue_name
   location            = var.environment.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_pep_id
 
   private_service_connection {
-    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-queue-pep-${var.environment.instance_number}"
+    name                           = local.storage_account.pep_queue_name
     private_connection_resource_id = azurerm_storage_account.this.id
     is_manual_connection           = false
     subresource_names              = ["queue"]
@@ -62,13 +62,13 @@ resource "azurerm_private_endpoint" "st_queue" {
 }
 
 resource "azurerm_private_endpoint" "function_sites" {
-  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-func-pep-${var.environment.instance_number}"
+  name                = local.function_app.pep_sites
   location            = var.environment.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_pep_id
 
   private_service_connection {
-    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-func-pep-${var.environment.instance_number}"
+    name                           = local.function_app.pep_sites
     private_connection_resource_id = azurerm_linux_function_app.this.id
     is_manual_connection           = false
     subresource_names              = ["sites"]
@@ -85,13 +85,13 @@ resource "azurerm_private_endpoint" "function_sites" {
 resource "azurerm_private_endpoint" "staging_function_sites" {
   count = local.function_app.is_slot_enabled
 
-  name                = "${local.project}-${var.environment.domain}-${var.environment.app_name}-staging-func-pep-${var.environment.instance_number}"
+  name                = local.function_app.pep_sites_staging
   location            = var.environment.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_pep_id
 
   private_service_connection {
-    name                           = "${local.project}-${var.environment.domain}-${var.environment.app_name}-staging-func-pep-${var.environment.instance_number}"
+    name                           = local.function_app.pep_sites_staging
     private_connection_resource_id = azurerm_linux_function_app.this.id
     is_manual_connection           = false
     subresource_names              = ["sites-${azurerm_linux_function_app_slot.this[0].name}"]
