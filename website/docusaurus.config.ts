23bd38a3a32068fb3081443a34271f5853ca2328
gunzip <gunzip@users.noreply.github.com>
2024-08-27 14:42:36 +0200

    [DEVEX-161] Cosmetic fixes to blog post (#81)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -106,6 +106,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ["hcl", "bash"],
     },
   } satisfies Preset.ThemeConfig,
 };
