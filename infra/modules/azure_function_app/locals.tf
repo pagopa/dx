957c217bf638203f6a66bef40f043e385cacbe77
Andrea Grillo <andrea.grillo@pagopa.it>
2024-09-06 11:33:21 +0200

    refactoring

--- infra/modules/azure_function_app/locals.tf
+++ infra/modules/azure_function_app/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  location_short = var.environment.location == "italynorth" ? "itn" : var.environment.location == "westeurope" ? "weu" : var.environment.location == "germanywestcentral" ? "gwc" : "neu"
-  project        = "${var.environment.prefix}-${var.environment.env_short}-${local.location_short}"
-  domain         = var.environment.domain == null ? "-" : "-${var.environment.domain}-"
+  location_short  = var.environment.location == "italynorth" ? "itn" : var.environment.location == "westeurope" ? "weu" : var.environment.location == "germanywestcentral" ? "gwc" : "neu"
+  project         = "${var.environment.prefix}-${var.environment.env_short}-${local.location_short}"
+  domain          = var.environment.domain == null ? "-" : "-${var.environment.domain}-"
+  app_name_prefix = "${local.project}${local.domain}${var.environment.app_name}"
 
   subnet = {
     enable_service_endpoints = var.subnet_service_endpoints != null ? concat(
@@ -9,22 +10,22 @@ locals {
       var.subnet_service_endpoints.web ? ["Microsoft.Web"] : [],
       var.subnet_service_endpoints.storage ? ["Microsoft.Storage"] : [],
     ) : []
-    name = "${local.project}${local.domain}${var.environment.app_name}-func-snet-${var.environment.instance_number}"
+    name = "${local.app_name_prefix}-func-snet-${var.environment.instance_number}"
   }
 
   app_service_plan = {
     enable = var.app_service_plan_id == null
-    name   = "${local.project}${local.domain}${var.environment.app_name}-asp-${var.environment.instance_number}"
+    name   = "${local.app_name_prefix}-asp-${var.environment.instance_number}"
   }
 
   function_app = {
-    name                   = "${local.project}${local.domain}${var.environment.app_name}-func-${var.environment.instance_number}"
+    name                   = "${local.app_name_prefix}-func-${var.environment.instance_number}"
     sku_name               = var.tier == "test" ? "B1" : var.tier == "standard" ? "P0v3" : "P1v3"
     zone_balancing_enabled = var.tier != "test"
     is_slot_enabled        = var.tier == "test" ? 0 : 1
-    pep_sites              = "${local.project}${local.domain}${var.environment.app_name}-func-pep-${var.environment.instance_number}"
-    pep_sites_staging      = "${local.project}${local.domain}${var.environment.app_name}-staging-func-pep-${var.environment.instance_number}"
-    alert                  = "${local.project}${local.domain}${var.environment.app_name}-func-${var.environment.instance_number}] Health Check Failed"
+    pep_sites              = "${local.app_name_prefix}-func-pep-${var.environment.instance_number}"
+    pep_sites_staging      = "${local.app_name_prefix}-staging-func-pep-${var.environment.instance_number}"
+    alert                  = "${local.app_name_prefix}-func-${var.environment.instance_number}] Health Check Failed"
   }
 
   function_app_slot = {
@@ -38,9 +39,9 @@ locals {
   storage_account = {
     replication_type = var.tier == "test" ? "LRS" : "ZRS"
     name             = replace("${local.project}${replace(local.domain, "-", "")}${var.environment.app_name}stfn${var.environment.instance_number}", "-", "")
-    pep_blob_name    = "${local.project}${local.domain}${var.environment.app_name}-blob-pep-${var.environment.instance_number}"
-    pep_file_name    = "${local.project}${local.domain}${var.environment.app_name}-file-pep-${var.environment.instance_number}"
-    pep_queue_name   = "${local.project}${local.domain}${var.environment.app_name}-queue-pep-${var.environment.instance_number}"
+    pep_blob_name    = "${local.app_name_prefix}-blob-pep-${var.environment.instance_number}"
+    pep_file_name    = "${local.app_name_prefix}-file-pep-${var.environment.instance_number}"
+    pep_queue_name   = "${local.app_name_prefix}-queue-pep-${var.environment.instance_number}"
     alert            = "[${replace("${local.project}${replace(local.domain, "-", "")}${var.environment.app_name}stfn${var.environment.instance_number}", "-", "")}] Low Availability"
   }
 
