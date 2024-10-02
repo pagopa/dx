18017095a296df13dcdc53d433523d1662ab9f4d
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-02 09:32:59 +0200

    docs: added comment in data

--- infra/modules/azure_storage_account/data.tf
+++ infra/modules/azure_storage_account/data.tf
@@ -1,3 +1,5 @@
+# This data block retrieves information only about Azure Private DNS Zones
+# regarding the subservices enabled by the user in var.subservices_enabled
 data "azurerm_private_dns_zone" "storage_account" {
   for_each            = { for subservice, status in var.subservices_enabled : subservice => status if status == true }
   name                = local.peps[each.key].dns_zone
