4d37f24d081f41e04ce98d4e01e01846511de2a6
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-03 09:16:27 +0200

    fix: removed provider configuration from modules

--- infra/modules/azure_function_app/main.tf
+++ infra/modules/azure_function_app/main.tf
@@ -1,7 +1,10 @@
-provider "azurerm" {
-  features {}
-
-  storage_use_azuread = true
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.100.0"
+    }
+  }
 }
 
 module "naming_convention" {
