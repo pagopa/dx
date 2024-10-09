edf44fa4078c0912e3f4ef20ff375604c56c5738
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-09 15:16:12 +0200

    feat: adopted new naming convention module

--- infra/modules/azure_cosmos_account/locals.tf
+++ infra/modules/azure_cosmos_account/locals.tf
@@ -1,13 +1,3 @@
 locals {
-  location_map = {
-    "italynorth"         = "itn"
-    "westeurope"         = "weu"
-    "germanywestcentral" = "gwc"
-  }
-  location_short  = lookup(local.location_map, var.environment.location, "itn")
-  project         = "${var.environment.prefix}-${var.environment.env_short}-${local.location_short}"
-  domain          = var.environment.domain == null ? "-" : "-${var.environment.domain}-"
-  app_name_prefix = "${local.project}${local.domain}${var.environment.app_name}"
-
   primary_location = var.primary_geo_location.location == null ? var.environment.location : var.primary_geo_location.location
 }
