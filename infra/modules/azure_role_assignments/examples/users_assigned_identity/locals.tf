9a336e7fad095024bd4457abb3e7525235392d06
Mario Mupo <mario.mupo@pagopa.it>
2024-11-05 11:48:15 +0100

    fix: minor

--- infra/modules/azure_role_assignments/examples/users_assigned_identity/locals.tf
+++ infra/modules/azure_role_assignments/examples/users_assigned_identity/locals.tf
@@ -8,15 +8,8 @@ locals {
     instance_number = "01"
   }
 
-  location_short  = local.environment.location == "italynorth" ? "itn" : local.environment.location == "westeurope" ? "weu" : local.environment.location == "germanywestcentral" ? "gwc" : "neu"
-  project         = "${local.environment.prefix}-${local.environment.env_short}-${local.location_short}"
-  resource_prefix = "${local.project}-${local.environment.domain}-${local.environment.app_name}"
+  location_short = local.environment.location == "italynorth" ? "itn" : local.environment.location == "westeurope" ? "weu" : local.environment.location == "germanywestcentral" ? "gwc" : "neu"
+  project        = "${local.environment.prefix}-${local.environment.env_short}-${local.location_short}"
 
-  tags = {
-    CreatedBy   = "Terraform"
-    Environment = "Dev"
-    Owner       = "DevEx"
-    Source      = "https://github.com/pagopa/dx/modules/azure_role_assignments/examples/complete"
-    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
-  }
+
 }
--- infra/resources/dev/locals.tf
+++ infra/resources/dev/locals.tf
@@ -1,12 +1,12 @@
-locals {
-  env_short = "d"
-  location  = "westeurope"
+# locals {
+#   env_short = "d"
+#   location  = "westeurope"
 
-  tags = {
-    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
-    CreatedBy   = "Terraform"
-    Environment = "Dev"
-    Owner       = "DevEx"
-    Source      = "https://github.com/pagopa/dx/blob/main/infra/resources/dev"
-  }
-}
+#   tags = {
+#     CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+#     CreatedBy   = "Terraform"
+#     Environment = "Dev"
+#     Owner       = "DevEx"
+#     Source      = "https://github.com/pagopa/dx/blob/main/infra/resources/dev"
+#   }
+# }
