f451c85f73b653732ef1b60fe30dd4c82d9b35c6
christian-calabrese <christian.calabrese@pagopa.it>
2024-11-07 17:01:03 +0100

    feat: allow system assigned identity to use storage account cmk

--- infra/modules/azure_storage_account/locals.tf
+++ infra/modules/azure_storage_account/locals.tf
@@ -38,4 +38,18 @@ locals {
       dns_zone = "privatelink.table.core.windows.net"
     }
   }
+
+  cmk_flags = {
+    kv = (var.customer_managed_key.enabled && var.customer_managed_key.type == "kv")
+  }
+
+  cmk_info = {
+    kv = local.cmk_flags.kv ? {
+      key_vault_name = split("/", var.customer_managed_key.key_vault_id)[8]
+      resource_group_name = split("/", var.customer_managed_key.key_vault_id)[4]
+      subscription = split("/", var.customer_managed_key.key_vault_id)[1]
+      same_subscription = split("/", var.customer_managed_key.key_vault_id)[1] == data.azurerm_subscription.current.subscription_id
+      principal_id = coalesce(var.customer_managed_key.user_assigned_identity_id, azurerm_storage_account.this.identity[0].principal_id)
+    } : {}
+  }
 }
