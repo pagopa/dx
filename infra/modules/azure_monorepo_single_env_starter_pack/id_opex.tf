2f363830bfd1f6f3a45d197352284dd14c679710
Andrea Grillo <andrea.grillo@outlook.com>
2024-11-13 10:41:03 +0100

    changes after PR

--- infra/modules/azure_monorepo_single_env_starter_pack/id_opex.tf
+++ infra/modules/azure_monorepo_single_env_starter_pack/id_opex.tf
@@ -36,40 +36,47 @@ resource "azurerm_role_assignment" "opex_ci_subscription_reader" {
   scope                = var.subscription_id
   role_definition_name = "Reader"
   principal_id         = azurerm_user_assigned_identity.opex_ci.principal_id
+  description          = "Allow ${var.repository.name} Opex CI identity to read resources at subscription scope"
 }
 
-resource "azurerm_role_assignment" "opex_ci_tf_rg_blob_reader" {
+resource "azurerm_role_assignment" "opex_ci_tf_rg_blob_contributor" {
   scope                = local.tf_storage_account.id
-  role_definition_name = "Storage Blob Data Reader"
+  role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.opex_ci.principal_id
+  description          = "Allow ${var.repository.name} Opex CI identity to apply changes to the Terraform state file Storage Account scope"
 }
 
 resource "azurerm_role_assignment" "opex_ci_subscription_data_access" {
   scope                = var.subscription_id
   role_definition_name = "Reader and Data Access"
   principal_id         = azurerm_user_assigned_identity.opex_ci.principal_id
+  description          = "Allow ${var.repository.name} Opex CI identity to read resources' keys and data at subscription scope"
 }
 
 resource "azurerm_role_assignment" "opex_cd_subscription_reader" {
   scope                = var.subscription_id
   role_definition_name = "Reader"
   principal_id         = azurerm_user_assigned_identity.opex_cd.principal_id
+  description          = "Allow ${var.repository.name} Opex CD identity to read resources at subscription scope"
 }
 
 resource "azurerm_role_assignment" "opex_cd_tf_rg_blob_contributor" {
   scope                = local.tf_storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.opex_cd.principal_id
+  description          = "Allow ${var.repository.name} Opex CD identity to apply changes to the Terraform state file Storage Account scope"
 }
 
 resource "azurerm_role_assignment" "opex_cd_tf_rg_blob_data_access" {
   scope                = local.tf_storage_account.id
   role_definition_name = "Reader and Data Access"
   principal_id         = azurerm_user_assigned_identity.opex_cd.principal_id
+  description          = "Allow ${var.repository.name} Opex CD identity to read resources' keys at the Terraform state file Storage Account scope"
 }
 
 resource "azurerm_role_assignment" "opex_cd_rg_opex_contributor" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "PagoPA Opex Dashboards Contributor"
   principal_id         = azurerm_user_assigned_identity.opex_cd.principal_id
+  description          = "Allow ${var.repository.name} Opex CD identity to apply changes to Opex dashboards at monorepository resource group scope"
 }
