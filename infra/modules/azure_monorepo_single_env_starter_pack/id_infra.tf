716a96a7c5d96642a19ebc187cbcb67a86c75cdc
Andrea Grillo <andrea.grillo@outlook.com>
2024-11-13 10:41:03 +0100

    changes after PR

--- infra/modules/azure_monorepo_single_env_starter_pack/id_infra.tf
+++ infra/modules/azure_monorepo_single_env_starter_pack/id_infra.tf
@@ -36,89 +36,103 @@ resource "azurerm_role_assignment" "infra_ci_subscription_reader" {
   scope                = var.subscription_id
   role_definition_name = "Reader"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to read resources at subscription scope"
 }
 
 resource "azurerm_role_assignment" "infra_ci_subscription_data_access" {
   scope                = var.subscription_id
   role_definition_name = "Reader and Data Access"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to read resources' keys and data at subscription scope"
 }
 
 resource "azurerm_role_assignment" "infra_ci_subscription_pagopa_iac_reader" {
   scope                = var.subscription_id
   role_definition_name = "PagoPA IaC Reader"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to read resources configuration at subscription scope"
 }
 
 resource "azurerm_role_assignment" "infra_ci_subscription_cosmos_contributor" {
   scope                = var.subscription_id
   role_definition_name = "DocumentDB Account Contributor"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to read Cosmos DB configuration at subscription scope"
 }
 
 resource "azurerm_role_assignment" "infra_ci_tf_st_blob_contributor" {
   scope                = local.tf_storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to apply changes to the Terraform state file Storage Account scope"
 }
 
 resource "azurerm_role_assignment" "infra_ci_rg_kv_secr" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to read KeyVault's secrets at monorepository resource group scope"
 }
 
 resource "azurerm_role_assignment" "infra_ci_rg_kv_cert" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Key Vault Certificate User"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to read KeyVault's certificates at monorepository resource group scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_subscription_reader" {
   scope                = var.subscription_id
   role_definition_name = "Reader"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CD identity to read resources at subscription scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_rg_contributor" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CD identity to apply changes to resources at monorepository resource group scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_vnet_network_contributor" {
   scope                = var.pep_vnet_id
   role_definition_name = "Network Contributor"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CD identity to manage Private Endpoints at VNet scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_apim_service_contributor" {
   scope                = var.apim_id
   role_definition_name = "API Management Service Contributor"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CD identity to manage configuration at APIM scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_st_tf_blob_contributor" {
   scope                = local.tf_storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CD identity to apply changes to the Terraform state file Storage Account scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_rg_rbac_admin" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Role Based Access Control Administrator"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CD identity to manage IAM configuration at monorepository resource group scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_rg_kv_secr" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to changes to KeyVault's secrets at monorepository resource group scope"
 }
 
 resource "azurerm_role_assignment" "infra_cd_rg_kv_cert" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Key Vault Certificates Officer"
   principal_id         = azurerm_user_assigned_identity.infra_cd.principal_id
+  description          = "Allow ${var.repository.name} Infra CI identity to change KeyVault's certificates at monorepository resource group scope"
 }
-
