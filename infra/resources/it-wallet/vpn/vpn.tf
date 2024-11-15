da3f469d8c70d02d1f383f0fa601dc5818ae476a
Krusty93 <andera.grillo@pagopa.it>
2024-11-15 09:52:34 +0100

    few fixes

--- infra/resources/it-wallet/vpn/vpn.tf
+++ infra/resources/it-wallet/vpn/vpn.tf
@@ -1,7 +1,7 @@
 module "vpn_snet" {
   source                                    = "github.com/pagopa/terraform-azurerm-v3//subnet?ref=v8.33.1"
   name                                      = "GatewaySubnet"
-  address_prefixes                          = ["10.2.252.8/29"]
+  address_prefixes                          = ["10.20.252.0/27"]
   resource_group_name                       = "${local.project}-networking-rg-01"
   virtual_network_name                      = "${local.project}-vnet-01"
   service_endpoints                         = []
@@ -11,16 +11,17 @@ module "vpn_snet" {
 module "vpn" {
   source = "github.com/pagopa/terraform-azurerm-v3//vpn_gateway?ref=v8.33.0"
 
-  name                = "${local.project}-vgw-01"
-  location            = "italynorth"
-  resource_group_name = "${local.project}-networking-rg-01"
-  sku                 = "VpnGw1"
-  pip_sku             = "Basic"
-  subnet_id           = module.vpn_snet.id
+  name                  = "${local.project}-vgw-01"
+  location              = "italynorth"
+  resource_group_name   = "${local.project}-networking-rg-01"
+  sku                   = "VpnGw1"
+  pip_sku               = "Standard"
+  pip_allocation_method = "Static"
+  subnet_id             = module.vpn_snet.id
 
   vpn_client_configuration = [
     {
-      address_space         = ["172.16.2.0/24"],
+      address_space         = ["172.16.2.0/27"],
       vpn_client_protocols  = ["OpenVPN"],
       aad_audience          = data.azuread_application.vpn_app_01.client_id
       aad_issuer            = "https://sts.windows.net/${data.azurerm_subscription.current.tenant_id}/"
@@ -38,7 +39,7 @@ module "vpn" {
 module "dns_forwarder_snet" {
   source                                    = "github.com/pagopa/terraform-azurerm-v3//subnet?ref=v8.33.1"
   name                                      = "${local.project}-dns-forwarder-snet-01"
-  address_prefixes                          = ["10.2.252.8/29"]
+  address_prefixes                          = ["10.20.254.8/29"]
   resource_group_name                       = "${local.project}-networking-rg-01"
   virtual_network_name                      = "${local.project}-vnet-01"
   private_endpoint_network_policies_enabled = false
