47782d4d5e2ef405a2d0aee9eb9918d8261bcf26
Mario Mupo <mario.mupo@pagopa.it>
2024-12-03 11:23:24 +0100

    fix: minor

--- infra/modules/azure_function_app_exposed/app_service_plan.tf
+++ infra/modules/azure_function_app_exposed/app_service_plan.tf
@@ -1,12 +1,12 @@
 resource "azurerm_service_plan" "this" {
   count = local.app_service_plan.enable ? 1 : 0
 
-  name                = local.app_service_plan.name
-  location            = var.environment.location
-  resource_group_name = var.resource_group_name
-  os_type             = "Linux"
-  sku_name            = local.function_app.sku_name
-  # zone_balancing_enabled = local.function_app.zone_balancing_enabled
+  name                   = local.app_service_plan.name
+  location               = var.environment.location
+  resource_group_name    = var.resource_group_name
+  os_type                = "Linux"
+  sku_name               = local.function_app.sku_name
+  zone_balancing_enabled = local.function_app.zone_balancing_enabled
 
   tags = var.tags
 }
