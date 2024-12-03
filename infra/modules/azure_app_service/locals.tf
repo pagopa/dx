857969126be9db07a4efd6ae92e3dc3152e223db
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2024-12-03 14:51:09 +0100

    [FIX] Updated App Service and Function App tier configuration (#171)

--- infra/modules/azure_app_service/locals.tf
+++ infra/modules/azure_app_service/locals.tf
@@ -16,6 +16,7 @@ locals {
     sku_name               = local.sku_name_mapping[local.tier]
     zone_balancing_enabled = local.tier != "s" && local.tier != "xs"
     is_slot_enabled        = local.tier == "s" || local.tier == "xs" ? 0 : 1
+    always_on              = local.tier == "xs" ? false : true
   }
 
   app_service_slot = {
--- infra/modules/azure_app_service_exposed/locals.tf
+++ infra/modules/azure_app_service_exposed/locals.tf
@@ -8,6 +8,7 @@ locals {
     sku_name               = local.sku_name_mapping[local.tier]
     zone_balancing_enabled = local.tier != "s" && local.tier != "xs"
     is_slot_enabled        = local.tier == "s" || local.tier == "xs" ? 0 : 1
+    always_on              = local.tier == "xs" ? false : true
   }
 
   application_insights = {
