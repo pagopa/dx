fda862e8a915a86b4b6b1d2e5793cba159fe04f7
Krusty93 <andera.grillo@pagopa.it>
2024-12-13 14:46:28 +0100

    fix event hub

--- infra/modules/azure_event_hub/tests/setup/main.tf
+++ infra/modules/azure_event_hub/tests/setup/main.tf
@@ -14,7 +14,7 @@ variable "project" {
 data "azurerm_subnet" "pep" {
   name                 = "${var.project}-pep-snet-01"
   virtual_network_name = "${var.project}-common-vnet-01"
-  resource_group_name  = "${var.project}-common-rg-01"
+  resource_group_name  = "${var.project}-network-rg-01"
 }
 
 output "pep_id" {
