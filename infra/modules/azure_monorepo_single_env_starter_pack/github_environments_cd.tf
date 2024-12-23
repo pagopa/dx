10dff201c5dd371b4bc013c894ae512449dcf0b5
Krusty93 <andera.grillo@pagopa.it>
2024-12-23 16:15:19 +0100

    add support for deployment policies

--- infra/modules/azure_monorepo_single_env_starter_pack/github_environments_cd.tf
+++ infra/modules/azure_monorepo_single_env_starter_pack/github_environments_cd.tf
@@ -52,22 +52,52 @@ resource "github_repository_environment" "opex_prod_cd" {
   }
 }
 
-resource "github_repository_environment_deployment_policy" "infra_prod_cd" {
+resource "github_repository_environment_deployment_policy" "infra_prod_cd_branch" {
+  for_each = var.repository.infra_cd_policy_branches
+
   repository     = github_repository.this.name
   environment    = github_repository_environment.infra_prod_cd.environment
-  branch_pattern = github_branch_default.main.branch
+  branch_pattern = each.value
 }
 
-resource "github_repository_environment_deployment_policy" "app_prod_cd" {
+resource "github_repository_environment_deployment_policy" "app_prod_cd_branch" {
+  for_each = var.repository.app_cd_policy_branches
+
   repository     = github_repository.this.name
   environment    = github_repository_environment.app_prod_cd.environment
-  branch_pattern = github_branch_default.main.branch
+  branch_pattern = each.value
 }
 
-resource "github_repository_environment_deployment_policy" "opex_prod_cd" {
+resource "github_repository_environment_deployment_policy" "opex_prod_cd_branch" {
+  for_each = var.repository.opex_cd_policy_branches
+
   repository     = github_repository.this.name
   environment    = github_repository_environment.opex_prod_cd.environment
-  branch_pattern = github_branch_default.main.branch
+  branch_pattern = each.value
+}
+
+resource "github_repository_environment_deployment_policy" "infra_prod_cd_tag" {
+  for_each = var.repository.infra_cd_policy_tags
+
+  repository     = github_repository.this.name
+  environment    = github_repository_environment.infra_prod_cd.environment
+  branch_pattern = each.value
+}
+
+resource "github_repository_environment_deployment_policy" "app_prod_cd_tag" {
+  for_each = var.repository.app_cd_policy_tags
+
+  repository     = github_repository.this.name
+  environment    = github_repository_environment.app_prod_cd.environment
+  branch_pattern = each.value
+}
+
+resource "github_repository_environment_deployment_policy" "opex_prod_cd_tag" {
+  for_each = var.repository.opex_cd_policy_tags
+
+  repository     = github_repository.this.name
+  environment    = github_repository_environment.opex_prod_cd.environment
+  branch_pattern = each.value
 }
 
 resource "github_actions_environment_secret" "infra_prod_cd" {
@@ -87,7 +117,6 @@ resource "github_actions_environment_secret" "app_prod_cd" {
   secret_name     = each.key
   plaintext_value = each.value
 }
-
 
 resource "github_actions_environment_secret" "opex_prod_cd" {
   for_each = local.opex_cd.secrets
