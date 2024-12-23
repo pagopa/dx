bcd9fb952b12fa7dd6a003c1ea44fbbdf3a46324
christian-calabrese <christian.calabrese@pagopa.it>
2024-12-23 16:12:46 +0100

    feat: allow override of gh runner labels to support multi env multi subscription cases

--- infra/modules/github_selfhosted_runner_on_container_app_jobs/container_app_job.tf
+++ infra/modules/github_selfhosted_runner_on_container_app_jobs/container_app_job.tf
@@ -31,7 +31,7 @@ resource "azurerm_container_app_job" "github_runner" {
           repos                     = var.repository.name
           targetWorkflowQueueLength = "1"
           github-runner             = "https://api.github.com"
-        }, var.container_app_environment.use_labels ? { labels = [local.env[var.environment.env_short]] } : {})
+        }, var.container_app_environment.use_labels ? { labels = coalesce(var.container_app_environment.override_labels, [local.env[var.environment.env_short]]) } : {})
 
         authentication {
           secret_name       = var.key_vault.secret_name
