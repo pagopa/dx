831cea133e8cc335fbe908ed379a8858344ee694
christian-calabrese <christian.calabrese@pagopa.it>
2024-12-23 18:32:37 +0100

    chore: use local for labels

--- infra/modules/github_selfhosted_runner_on_container_app_jobs/locals.tf
+++ infra/modules/github_selfhosted_runner_on_container_app_jobs/locals.tf
@@ -9,4 +9,6 @@ locals {
     job_name            = "${module.naming_convention.prefix}-caj-${module.naming_convention.suffix}"
     resource_group_name = var.resource_group_name == null ? "${module.naming_convention.prefix}-github-runner-rg-01" : var.resource_group_name
   }
+
+  labels = coalescelist(var.container_app_environment.override_labels, [local.env[var.environment.env_short]])
 }
