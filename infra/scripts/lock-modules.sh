6aac4436f9dde47d678dc9ca5862a240b22ff1c0
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-02 11:32:29 +0100

    fix: debug jq version

--- infra/scripts/lock-modules.sh
+++ infra/scripts/lock-modules.sh
@@ -133,10 +133,10 @@ function process_module() {
     local previous_hash
     
     init_hashes_file "$HASHES_FILE"
+    info $(jq --version)
 
     # Get previous hash from hashes file
     previous_hash=$(jq -r --arg module "$module_name" '.[$module] // "none"' "${HASHES_FILE:-/dev/null}")
-    info $(jq --version)
     # Update hash in hashes file
     jq --arg module "$module_name" --arg hash "$new_hash" '.[$module] = $hash' \
         "$HASHES_FILE" > "tmp.$$.json" && mv "tmp.$$.json" "$HASHES_FILE"
