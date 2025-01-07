5c46e66855ca2f995798a6bede757408c9c091ba
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-07 14:26:17 +0100

    Fix java_server_version syntax in azure_app_service modules (#197)

--- infra/modules/azure_app_service_exposed/app_service_slot.tf
+++ infra/modules/azure_app_service_exposed/app_service_slot.tf
@@ -23,7 +23,7 @@ resource "azurerm_linux_web_app_slot" "this" {
       node_version        = var.stack == "node" ? "${var.node_version}-lts" : null
       java_version        = var.stack == "java" ? var.java_version : null
       java_server         = var.stack == "java" ? "JAVA" : null
-      java_server_version = var.stack == "java" ? "${var.java_version}-java${var.java_version}" : null
+      java_server_version = var.stack == "java" ? var.java_version : null
     }
   }
 
