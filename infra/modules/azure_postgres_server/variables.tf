e92267652d06d3068f0eafa68ec31f514d44f3fd
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-27 11:05:00 +0100

    feat: add lock to psql flexible server

--- infra/modules/azure_postgres_server/variables.tf
+++ infra/modules/azure_postgres_server/variables.tf
@@ -262,3 +262,9 @@ variable "diagnostic_settings" {
     error_message = "log_analytics_workspace_id and diagnostic_setting_destination_storage_id are mandatory if diagnostic is enabled."
   }
 }
+
+variable "needs_lock" {
+  type        = bool
+  default     = true
+  description = "Define if lock should be enabled."
+}
