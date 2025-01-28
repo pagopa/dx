5892d51b560f1b9d61b986a1f6826f4574fc087b
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-01-28 16:58:34 +0100

    feat: add azure-functions-core-tools feature

---
+++ .devcontainer/features/azure-functions-core-tools/install.sh
@@ -0,0 +1,28 @@
+echo "Choose the version of Azure Functions Core Tools to install"
+if [ "$VERSION" = "latest" ]; then
+    CORE_TOOLS_VERSION=$(curl -s https://api.github.com/repos/Azure/azure-functions-core-tools/releases/latest | jq -r '.tag_name')
+else
+    CORE_TOOLS_VERSION=$VERSION
+fi
+
+BUILD_NUMBER=$(echo $CORE_TOOLS_VERSION | cut -d '.' -f 3)
+
+echo "Clone Azure Functions Core Tools repository"
+git clone --depth 1 --branch $CORE_TOOLS_VERSION https://github.com/Azure/azure-functions-core-tools.git
+
+cd azure-functions-core-tools
+
+echo "Build Azure Functions Core Tools"
+dotnet publish src/Azure.Functions.Cli/Azure.Functions.Cli.csproj \
+ /p:BuildNumber=$BUILD_NUMBER \
+ --use-current-runtime \
+ --framework=net8.0 \
+ --output /opt/azure-functions-core-tools
+
+echo "Create a symbolic link to the func binary"
+ln -s /opt/azure-functions-core-tools/func /usr/local/bin/func
+chmod +x /opt/azure-functions-core-tools/func
+
+echo "Cleanup"
+cd ..
+rm -rf azure-functions-core-tools
