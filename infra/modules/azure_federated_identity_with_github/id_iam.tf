c103d23b140af0c571f747a242fd73db44dd08c6
Andrea Grillo <andrea.grillo@pagopa.it>
2025-02-25 17:12:28 +0100

    [CES-759] Remove `terraform-azurerm-v4` dependency in  `azure_federated_identity_with_github` module (#288)
    Co-authored-by: Mario Mupo <43968294+mamu0@users.noreply.github.com>

---
+++ infra/modules/azure_federated_identity_with_github/id_iam.tf
@@ -0,0 +1,31 @@
+resource "azurerm_role_assignment" "ci_subscription" {
+  for_each = (!var.continuos_integration.enable || var.continuos_integration.roles == null) ? toset([]) : var.continuos_integration.roles.subscription
+
+  scope                = var.subscription_id
+  role_definition_name = each.value
+  principal_id         = azurerm_user_assigned_identity.ci[0].principal_id
+}
+
+resource "azurerm_role_assignment" "cd_subscription" {
+  for_each = (!var.continuos_delivery.enable || var.continuos_delivery.roles == null) ? toset([]) : var.continuos_delivery.roles.subscription
+
+  scope                = var.subscription_id
+  role_definition_name = each.value
+  principal_id         = azurerm_user_assigned_identity.cd[0].principal_id
+}
+
+resource "azurerm_role_assignment" "ci_rg" {
+  count = length(local.ci_rg_roles)
+
+  scope                = local.ci_rg_roles[count.index].resource_group_id
+  role_definition_name = local.ci_rg_roles[count.index].role_name
+  principal_id         = azurerm_user_assigned_identity.ci[0].principal_id
+}
+
+resource "azurerm_role_assignment" "cd_rg" {
+  count = length(local.cd_rg_roles)
+
+  scope                = local.cd_rg_roles[count.index].resource_group_id
+  role_definition_name = local.cd_rg_roles[count.index].role_name
+  principal_id         = azurerm_user_assigned_identity.cd[0].principal_id
+}
--- infra/modules/azure_federated_identity_with_github/locals.tf
+++ infra/modules/azure_federated_identity_with_github/locals.tf
@@ -1,15 +1,43 @@
 locals {
-  ci_github_federations = tolist(flatten([
-    for repo in var.repositories : {
-      repository = repo
-      subject    = "${var.env}-ci"
-    }
-  ]))
 
-  cd_github_federations = tolist(flatten([
-    for repo in var.repositories : {
-      repository = repo
-      subject    = "${var.env}-cd"
-    }
-  ]))
+  resource_group_name = var.resource_group_name
+  #e.g. dx-d-itn-test-app-github-cd-id-01
+  identity_name = "${module.naming_convention.prefix}-%s-github-%s-id-${module.naming_convention.suffix}"
+  #e.g. dx-environment-app-prod-cd
+  federation_prefix = "${var.repository.name}-environment-%s-${module.naming_convention.env_name}-%s"
+
+  is_ci_enabled = var.continuos_integration.enable ? 1 : 0
+  is_cd_enabled = var.continuos_delivery.enable ? 1 : 0
+
+  ci_github_federations = local.is_ci_enabled == 1 ? {
+    audience = ["api://AzureADTokenExchange"]
+    issuer   = "https://token.actions.githubusercontent.com"
+    # repo:pagopa/dx:environment:app-prod-ci
+    subject = format("repo:${var.repository.owner}/${var.repository.name}:environment:%s-${module.naming_convention.env_name}-ci", var.identity_type)
+  } : null
+
+  cd_github_federations = local.is_cd_enabled == 1 ? {
+    audience = ["api://AzureADTokenExchange"]
+    issuer   = "https://token.actions.githubusercontent.com"
+    # repo:pagopa/dx:environment:app-prod-cd
+    subject = format("repo:${var.repository.owner}/${var.repository.name}:environment:%s-${module.naming_convention.env_name}-cd", var.identity_type)
+  } : null
+
+  ci_rg_roles = var.continuos_integration.enable ? tolist(flatten([
+    for rg in data.azurerm_resource_group.ci_details : [
+      for role in var.continuos_integration.roles.resource_groups[rg.name] : {
+        resource_group_id = rg.id
+        role_name         = role
+      }
+    ]
+  ])) : []
+
+  cd_rg_roles = var.continuos_delivery.enable ? tolist(flatten([
+    for rg in data.azurerm_resource_group.cd_details : [
+      for role in var.continuos_delivery.roles.resource_groups[rg.name] : {
+        resource_group_id = rg.id
+        role_name         = role
+      }
+    ]
+  ])) : []
 }
