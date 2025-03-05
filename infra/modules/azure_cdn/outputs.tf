64d8627b6174dcd457e4de197ea7ca0c0c5db3f9
Christian Calabrese <christian.calabrese@pagopa.it>
2025-03-05 15:49:50 +0100

    Add name and resource group name as outputs of the module

--- infra/modules/azure_cdn/outputs.tf
+++ infra/modules/azure_cdn/outputs.tf
@@ -3,6 +3,16 @@ output "id" {
   value       = azurerm_cdn_frontdoor_profile.this.id
 }
 
+output "name" {
+  description = "The name of the CDN FrontDoor Profile"
+  value       = azurerm_cdn_frontdoor_profile.this.name
+}
+
+output "resource_group_name" {
+  description = "The name of the resource group the CDN FrontDoor Profile was created in"
+  value       = azurerm_cdn_frontdoor_profile.this.resource_group_name
+}
+
 output "endpoint_hostname" {
   description = "The hostname of the CDN FrontDoor Endpoint"
   value       = azurerm_cdn_frontdoor_endpoint.this.host_name
