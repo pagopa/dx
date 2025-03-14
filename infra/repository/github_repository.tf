da0405eabe0affa27192cb40666b7280966febc9
Marco Comi <marco.comi@pagopa.it>
2025-03-14 15:48:18 +0100

    Enable autolink

--- infra/repository/github_repository.tf
+++ infra/repository/github_repository.tf
@@ -29,5 +29,12 @@ resource "github_repository" "this" {
       path   = "/"
     }
   }
+}
 
+resource "github_repository_autolink_reference" "jira" {
+  for_each = toset(local.jira_boards)
+
+  key_prefix          = each.value
+  repository          = github_repository.this.name
+  target_url_template = "https://pagopa.atlassian.net/browse/${each.value}-<num>"
 }
