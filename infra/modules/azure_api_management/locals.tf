605726f1815f6e9b2fa82eced1ac0d0faab55a03
Mario Mupo <mario.mupo@pagopa.it>
2025-03-21 15:50:42 +0100

    fix: Changed how define apim name

--- infra/modules/azure_api_management/locals.tf
+++ infra/modules/azure_api_management/locals.tf
@@ -1,7 +1,8 @@
 locals {
+  prefix = var.environment.app_name != "apim" ? module.naming_convention.prefix : var.environment.domain != null ? "${module.naming_convention.project}-${var.environment.domain}" : module.naming_convention.project
   apim = {
-    name           = "${module.naming_convention.prefix}-apim-${module.naming_convention.suffix}"
-    autoscale_name = var.tier == "l" ? "${module.naming_convention.prefix}-apim-as-${module.naming_convention.suffix}" : null
+    name           = "${local.prefix}-apim-${module.naming_convention.suffix}"
+    autoscale_name = var.tier == "l" ? "${local.prefix}-apim-as-${module.naming_convention.suffix}" : null
     zones          = length(var.zones_override) > 0 ? var.zones_override : ["1", "2", "3"]
     sku_name = lookup(
       {
