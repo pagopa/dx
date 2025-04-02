1a9184596fe4dc7705407d2b6cf418455c581244
Christian Calabrese <christian.calabrese@pagopa.it>
2025-04-02 17:19:55 +0200

    Start using github_environment_bootstrap module

--- infra/modules/azure_github_environment_bootstrap/github_repository_secrets.tf
+++ infra/modules/azure_github_environment_bootstrap/github_repository_secrets.tf
@@ -1,7 +1,7 @@
 resource "github_actions_secret" "repo_secrets" {
   for_each = local.repo_secrets
 
-  repository      = github_repository.this.name
+  repository      = local.repository_name
   secret_name     = each.key
   plaintext_value = each.value
 }
