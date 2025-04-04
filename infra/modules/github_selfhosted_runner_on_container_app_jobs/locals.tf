48065d4aac48da1391dfd7cc9d2cb689ef202edc
Mario Mupo <mario.mupo@pagopa.it>
2025-04-04 17:05:28 +0200

    feat: Replace naming convention module with DX provider functions

--- infra/modules/github_selfhosted_runner_on_container_app_jobs/locals.tf
+++ infra/modules/github_selfhosted_runner_on_container_app_jobs/locals.tf
@@ -1,13 +1,22 @@
 locals {
+  naming_config = {
+    prefix          = var.environment.prefix,
+    environment     = var.environment.env_short,
+    location        = var.environment.location
+    name            = length(var.repository.name) > 16 ? substr(var.repository.name, 0, 16) : var.repository.name,
+    instance_number = tonumber(var.environment.instance_number),
+  }
   env = {
     "d" = "dev",
     "u" = "uat",
     "p" = "prod"
   }
-
   container_apps = {
-    job_name            = "${module.naming_convention.prefix}-caj-${module.naming_convention.suffix}"
-    resource_group_name = var.resource_group_name == null ? "${module.naming_convention.prefix}-github-runner-rg-01" : var.resource_group_name
+    job_name = provider::dx::resource_name(merge(local.naming_config, { resource_type = "container_app_job" }))
+    resource_group_name = var.resource_group_name == null ? provider::dx::resource_name(merge(local.naming_config, {
+      name          = "github-runner",
+      resource_type = "resource_group"
+    })) : var.resource_group_name
   }
 
   labels = join(",", coalescelist(var.container_app_environment.override_labels, [local.env[var.environment.env_short]]))
--- infra/modules/github_selfhosted_runner_on_container_app_jobs/main.tf
+++ infra/modules/github_selfhosted_runner_on_container_app_jobs/main.tf
@@ -5,19 +5,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.110, < 5.0"
     }
-  }
-}
-
-module "naming_convention" {
-  source  = "pagopa-dx/azure-naming-convention/azurerm"
-  version = "~> 0.0"
-
-  environment = {
-    prefix          = var.environment.prefix
-    env_short       = var.environment.env_short
-    location        = var.environment.location
-    domain          = null
-    app_name        = length(var.repository.name) > 16 ? substr(var.repository.name, 0, 16) : var.repository.name
-    instance_number = var.environment.instance_number
+    dx = {
+      source  = "pagopa-dx/azure"
+      version = ">= 0.0.6, < 1.0.0"
+    }
   }
 }
