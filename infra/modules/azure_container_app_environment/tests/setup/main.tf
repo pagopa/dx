b013061ec7e92e6cc4308d4ef949085b575b61a5
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2025-04-07 16:29:17 +0200

    Remove `naming_convention` inside Container App modules in favor of `pagopa-dx/azure` provider function (#420)
    This PR removes all instances of the naming_convention module from the
    Container App and Container App Environment modules. The functionality
    has been replaced with the new naming convention function provided by
    the pagopa-dx/azure provider.
    
    Resolves: CES-917

--- infra/modules/azure_container_app_environment/tests/setup/main.tf
+++ infra/modules/azure_container_app_environment/tests/setup/main.tf
@@ -4,35 +4,59 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.110, < 5.0"
     }
+    dx = {
+      source  = "pagopa-dx/azure"
+      version = ">= 0.0.6, < 1.0.0"
+    }
   }
 }
 
-module "naming_convention" {
-  source = "../../../azure_naming_convention"
-
-  environment = {
-    prefix          = var.environment.prefix
-    env_short       = var.environment.env_short
+locals {
+  naming_config = {
+    prefix          = var.environment.prefix,
+    environment     = var.environment.env_short,
     location        = var.environment.location
-    domain          = var.environment.domain
-    app_name        = var.environment.app_name
-    instance_number = var.environment.instance_number
+    name            = var.environment.app_name,
+    instance_number = tonumber(var.environment.instance_number),
+  }
+
+  virtual_network = {
+    name = provider::dx::resource_name(merge(local.naming_config, {
+      name          = "common",
+      resource_type = "virtual_network"
+    }))
+    resource_group_name = provider::dx::resource_name(merge(local.naming_config, {
+      name          = "network",
+      resource_type = "resource_group"
+    }))
   }
 }
 
 data "azurerm_resource_group" "rg" {
-  name = "${var.environment.prefix}-${var.environment.env_short}-itn-test-rg-${module.naming_convention.suffix}"
+  name = provider::dx::resource_name(merge(local.naming_config, {
+    name          = "test",
+    resource_type = "resource_group"
+  }))
 }
 
 data "azurerm_subnet" "pep" {
-  name                 = "${var.environment.prefix}-${var.environment.env_short}-itn-pep-snet-01"
-  virtual_network_name = "${var.environment.prefix}-${var.environment.env_short}-itn-common-vnet-01"
-  resource_group_name  = "${var.environment.prefix}-${var.environment.env_short}-itn-network-rg-01"
+  name = provider::dx::resource_name(merge(local.naming_config, {
+    name          = "pep",
+    resource_type = "subnet"
+  }))
+  virtual_network_name = local.virtual_network.name
+  resource_group_name  = local.virtual_network.resource_group_name
 }
 
 data "azurerm_log_analytics_workspace" "logs" {
-  name                = "${var.environment.prefix}-${var.environment.env_short}-itn-common-log-${module.naming_convention.suffix}"
-  resource_group_name = "${var.environment.prefix}-${var.environment.env_short}-itn-common-rg-01"
+  name = provider::dx::resource_name(merge(local.naming_config, {
+    name          = "common",
+    resource_type = "log_analytics"
+  }))
+  resource_group_name = provider::dx::resource_name(merge(local.naming_config, {
+    name          = "common",
+    resource_type = "resource_group"
+  }))
 }
 
 output "resource_group_name" {
