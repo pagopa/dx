ee1be39ec7a28fb479645094c998065e91072587
Mario Mupo <mario.mupo@pagopa.it>
2025-04-10 15:08:51 +0200

    fix: resolve comment

--- infra/modules/azure_app_service_plan/app_service_plan.tf
+++ infra/modules/azure_app_service_plan/app_service_plan.tf
@@ -1,13 +1,5 @@
 resource "azurerm_service_plan" "this" {
-  name = provider::dx::resource_name({
-    prefix          = var.environment.prefix,
-    environment     = var.environment.env_short,
-    location        = var.environment.location
-    domain          = var.environment.domain,
-    name            = var.environment.app_name,
-    instance_number = tonumber(var.environment.instance_number),
-    resource_type   = "app_service_plan"
-  })
+  name                   = provider::dx::resource_name(merge(local.naming_config, { resource_type = "app_service_plan" }))
   location               = var.environment.location
   resource_group_name    = var.resource_group_name
   os_type                = "Linux"
