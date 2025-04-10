ef5fb153e3ada7e48a8c22a5722568d5f65142d2
Marco Comi <marco.comi@pagopa.it>
2025-04-10 15:36:25 +0200

    Put undici instrumentation in a separate file

--- packages/azure-tracing/src/azure-monitor.ts
+++ packages/azure-tracing/src/azure-monitor.ts
@@ -4,7 +4,8 @@ import {
   Instrumentation,
   registerInstrumentations,
 } from "@opentelemetry/instrumentation";
-import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
+
+import { registerUndiciInstrumentation } from "./azure-undici-instrumentation";
 
 /**
  * Initialize the Azure Monitor with the given instrumentations.
@@ -30,23 +31,7 @@ export const initAzureMonitor = (
   registerInstrumentations({
     instrumentations: [
       // instrument native node fetch
-      new UndiciInstrumentation({
-        requestHook: (span, requestInfo) => {
-          const { method, origin, path } = requestInfo;
-          // Default instrumented attributes don't feed well into AppInsights,
-          // so we set them manually.
-          span.setAttributes({
-            "http.host": origin,
-            "http.method": method,
-            "http.target": path,
-            "http.url": `${origin}${path}`,
-          });
-        },
-        responseHook: (span, { response }) => {
-          // Same as above, set the status code manually.
-          span.setAttribute("http.status_code", response.statusCode);
-        },
-      }),
+      registerUndiciInstrumentation(),
       ...instrumentations,
     ],
     meterProvider: metrics.getMeterProvider(),
