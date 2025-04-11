3f4daeb84f4f6568eafc467a37696a75210a2952
Krusty93 <andrea.grillo@pagopa.it>
2025-04-11 10:22:43 +0200

    update name

--- infra/modules/azure_container_app_environment/id.tf
+++ infra/modules/azure_container_app_environment/id.tf
@@ -1,5 +1,5 @@
 resource "azurerm_user_assigned_identity" "cae_identity" {
-  name                = provider::dx::resource_name(merge(local.naming_config, { resource_type = "managed_identity" }))
+  name                = provider::dx::resource_name(merge(local.naming_config, { domain = "com", name = "cae", resource_type = "managed_identity" }))
   location            = var.environment.location
   resource_group_name = var.resource_group_name
 
