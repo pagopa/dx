750b1286d3b626d2d75c4fa55b6910c5ad8ae0cb
Krusty93 <andrea.grillo@pagopa.it>
2025-04-14 09:59:59 +0200

    add comma

--- infra/modules/github_selfhosted_runner_on_container_app_jobs/locals.tf
+++ infra/modules/github_selfhosted_runner_on_container_app_jobs/locals.tf
@@ -2,17 +2,19 @@ locals {
   naming_config = {
     prefix          = var.environment.prefix,
     environment     = var.environment.env_short,
-    location        = var.environment.location
+    location        = var.environment.location,
     name            = length(var.repository.name) > 16 ? substr(var.repository.name, 0, 16) : var.repository.name,
     instance_number = tonumber(var.environment.instance_number),
   }
+
   env = {
     "d" = "dev",
     "u" = "uat",
     "p" = "prod"
   }
+
   container_apps = {
-    job_name = provider::dx::resource_name(merge(local.naming_config, { resource_type = "container_app_job" }))
+    job_name = provider::dx::resource_name(merge(local.naming_config, { resource_type = "container_app_job" })),
     resource_group_name = var.resource_group_name == null ? provider::dx::resource_name(merge(local.naming_config, {
       name          = "github-runner",
       resource_type = "resource_group"
