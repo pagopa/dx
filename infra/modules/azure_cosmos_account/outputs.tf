ae69d0e0e40025ff44e309ef54d13f2d2df2c975
Mario Mupo <mario.mupo@pagopa.it>
2025-04-15 18:07:43 +0200

    fix: Update Cosmos variables and outputs descriptions

--- infra/modules/azure_cosmos_account/outputs.tf
+++ infra/modules/azure_cosmos_account/outputs.tf
@@ -1,23 +1,29 @@
 output "name" {
-  value = azurerm_cosmosdb_account.this.name
+  description = "The name of the Azure Cosmos DB account."
+  value       = azurerm_cosmosdb_account.this.name
 }
 
 output "id" {
-  value = azurerm_cosmosdb_account.this.id
+  description = "The ID of the Azure Cosmos DB account."
+  value       = azurerm_cosmosdb_account.this.id
 }
 
 output "resource_group_name" {
-  value = azurerm_cosmosdb_account.this.resource_group_name
+  description = "The name of the resource group containing the Azure Cosmos DB account."
+  value       = azurerm_cosmosdb_account.this.resource_group_name
 }
 
 output "endpoint" {
-  value = azurerm_cosmosdb_account.this.endpoint
+  description = "The primary endpoint URL of the Azure Cosmos DB account."
+  value       = azurerm_cosmosdb_account.this.endpoint
 }
 
 output "read_endpoints" {
-  value = azurerm_cosmosdb_account.this.read_endpoints
+  description = "A list of read endpoints for the Azure Cosmos DB account."
+  value       = azurerm_cosmosdb_account.this.read_endpoints
 }
 
 output "write_endpoints" {
-  value = azurerm_cosmosdb_account.this.write_endpoints
+  description = "A list of write endpoints for the Azure Cosmos DB account."
+  value       = azurerm_cosmosdb_account.this.write_endpoints
 }
