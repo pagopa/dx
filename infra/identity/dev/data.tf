3379968089decb135f8a0773cbb8b220eabf711b
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2025-05-08 17:59:22 +0200

    Align identity configuration (#544)

--- infra/identity/dev/data.tf
+++ infra/identity/dev/data.tf
@@ -1,3 +1,5 @@
+data "azurerm_subscription" "current" {}
+
 data "azurerm_key_vault" "common" {
   name                = "${local.prefix}-${local.env_short}-${local.location_short}-common-kv-01"
   resource_group_name = "${local.prefix}-${local.env_short}-${local.location_short}-common-rg-01"
