5beb2d0541219ad80639398ae698c0116eb0fc3c
Krusty93 <andrea.grillo@pagopa.it>
2025-05-09 15:35:16 +0200

    remove partitions and basic sku

--- infra/modules/azure_service_bus_namespace/locals.tf
+++ infra/modules/azure_service_bus_namespace/locals.tf
@@ -8,19 +8,16 @@ locals {
     instance_number = tonumber(var.environment.instance_number),
   }
 
-  namespace = {
-    name = provider::dx::resource_name(merge(local.naming_config, { reosurce_type = "servicebus_namespace" }))
-    sku_name = lookup(
-      {
-        "s" = "Standard",
-        "m" = "Standard",
-        "l" = "Premium"
-      },
-      var.tier,
-      "Premium" # Default
-    )
-    partitions = local.namespace.sku_name == "Premium" ? 1 : 0
-  }
+  name = provider::dx::resource_name(merge(local.naming_config, { reosurce_type = "servicebus_namespace" }))
+
+  sku_name = lookup(
+    {
+      "m" = "Standard",
+      "l" = "Premium"
+    },
+    var.tier,
+    "Premium"
+  )
 
   private_dns_zone_resource_group_name = var.private_dns_zone_resource_group_name == null ? var.resource_group_name : var.private_dns_zone_resource_group_name
 
