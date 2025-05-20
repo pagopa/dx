2f98e9d36cb651ce3a52e480bd465416523c9087
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-20 12:25:47 +0200

    Implement OpenNext lambda server module

---
+++ infra/modules/aws_open_next/modules/lambda/iam.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "${local.app_prefix}-lambda-role-${local.app_suffix}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+
+  tags = var.tags
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name   = "${local.app_prefix}-lambda-policy-${local.app_suffix}"
+  policy = data.aws_iam_policy_document.lambda_policy.json
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
