4ee4417e158dcde30938cb9878949e0e304eb389
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-21 13:56:19 +0200

    Add custom domain support and root module

---
+++ infra/modules/aws_open_next/outputs.tf
@@ -0,0 +1,24 @@
+output "server" {
+  value = module.server
+  description = "The server lambda function."
+}
+
+output "image_optimizer" {
+  value = module.image_optimizer
+  description = "The image optimizer lambda function."
+}
+
+output "isr_revalidation" {
+  value = module.isr_revalidation
+  description = "The ISR revalidation lambda function."
+}
+
+output "cloudfront" {
+  value = module.cloudfront
+  description = "The CloudFront distribution."
+}
+
+output "assets" {
+  value = module.assets
+  description = "The assets bucket."
+}
