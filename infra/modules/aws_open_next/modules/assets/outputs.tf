d238e8d9cd76ccfe951933ef1ed4944d2641c55f
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-21 12:20:19 +0200

    Add Cloudfront OAC to each origin

--- infra/modules/aws_open_next/modules/assets/outputs.tf
+++ infra/modules/aws_open_next/modules/assets/outputs.tf
@@ -7,7 +7,7 @@ output "bucket" {
 
 output "cloudfront_origin_access_control" {
   value = {
-    id = aws_cloudfront_origin_access_identity.assets.id
-    arn = aws_cloudfront_origin_access_identity.assets.arn
+    id = aws_cloudfront_origin_access_control.assets.id
+    arn = aws_cloudfront_origin_access_control.assets.arn
   }
 }
