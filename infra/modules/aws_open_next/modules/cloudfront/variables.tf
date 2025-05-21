4dc8dfb816b2bc805666ae7a3ee31b0cfedc79e2
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-21 13:08:34 +0200

    Add cloudfront distribution

---
+++ infra/modules/aws_open_next/modules/cloudfront/variables.tf
@@ -0,0 +1,65 @@
+#---------#
+# General #
+#---------#
+
+variable "tags" {
+  type        = map(any)
+  description = "Resources tags"
+}
+
+variable "environment" {
+  type = object({
+    prefix          = string
+    env_short       = string
+    location        = string
+    domain          = optional(string)
+    app_name        = string
+    instance_number = string
+  })
+
+  description = "Values which are used to generate resource names and location short names. They are all mandatory except for domain, which should not be used only in the case of a resource used by multiple domains."
+}
+
+variable "custom_domain" {
+  type = object({
+    domain_name       = string
+    zone_name         = string
+    certificate_arn   = string
+  })
+
+  description = "Custom domain information."
+  default     = null
+}
+
+variable "custom_headers" {
+  type        = list(object({
+    header   = string
+    value    = string
+    override = optional(bool)
+  }))
+  description = "Custom headers to be added to the CloudFront distribution."
+  default     = []
+}
+
+variable "origins" {
+  type = object({
+    assets_bucket = object({
+      domain_name = string
+      oai  = string
+    })
+    server_function = object({
+      url = string
+      oac = string
+    })
+    image_optimization_function = object({
+      url = string
+      oac = string
+    })
+  })
+}
+
+variable "enable_waf" {
+  type        = bool
+  description = "Enable WAF for CloudFront distribution."
+  default     = false
+}
---
+++ infra/modules/aws_open_next/modules/cloudfront/waf.tf
@@ -0,0 +1,122 @@
+resource "aws_wafv2_web_acl" "cloudfront" {
+  count = var.enable_waf == null ? 1 : 0
+
+  provider = aws.global
+  name     = "${local.app_prefix}-waf"
+  scope    = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+        rule_action_override {
+          action_to_use {
+            count {}
+          }
+
+          name = "NoUserAgent_HEADER"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.app_prefix}-WAF-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Bad Inputs Rule Set
+  # https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html#aws-managed-rule-groups-baseline-known-bad-inputs
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.app_prefix}-WAF-AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  # Linux Operating System Rule Group
+  # https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-use-case.html#aws-managed-rule-groups-use-case-linux-os
+  rule {
+    name     = "AWS-AWSManagedRulesLinuxRuleSet"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.app_prefix}-WAF-AWSManagedRulesLinuxRuleSet"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  # POSIX Operating System Rule Group
+  # https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-use-case.html#aws-managed-rule-groups-use-case-posix-os
+  rule {
+    name     = "AWS-AWSManagedRulesUnixRuleSet"
+    priority = 4
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesUnixRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.app_prefix}-WAF-AWSManagedRulesUnixRuleSet"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "${local.app_prefix}-WAF"
+    sampled_requests_enabled   = false
+  }
+}
+
