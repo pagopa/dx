57cc9c588430b0d8a3f26154ce801342f5d5f13b
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-23 23:30:16 +0200

    Add previews feature

--- infra/modules/aws_open_next/modules/server_lambda/warmer.tf
+++ infra/modules/aws_open_next/modules/server_lambda/warmer.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_event_rule" "scheduled_lambda_event_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
-  arn  = aws_lambda_function.function.arn
+  arn  = aws_lambda_alias.production.arn
   rule = aws_cloudwatch_event_rule.scheduled_lambda_event_rule.name
 }
 
@@ -14,5 +14,6 @@ resource "aws_lambda_permission" "allow_execution_from_eventbridge" {
   statement_id  = "AllowExecutionFromEventbridge"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.function.function_name
+  qualifier     = aws_lambda_alias.production.name
   principal     = "events.amazonaws.com"
 }
