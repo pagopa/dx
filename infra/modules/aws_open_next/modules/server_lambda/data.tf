a83219af23d253983586488a2b81b949bd8c8472
uolter <walter.traspadini@pagopa.it>
2025-05-28 15:54:33 +0200

    update lambda dummy archive, removed unused variable

--- infra/modules/aws_open_next/modules/server_lambda/data.tf
+++ infra/modules/aws_open_next/modules/server_lambda/data.tf
@@ -1,6 +1,27 @@
+locals {
+  source_files = ["${path.module}/../../dummy_lambda/index.js", "${path.module}/../../dummy_lambda/package.json"]
+}
+
+data "template_file" "t_file" {
+  count    = length(local.source_files)
+  template = file(element(local.source_files, count.index))
+}
+
+
 data "archive_file" "lambda_zip" {
   type = "zip"
 
   source_dir  = "${path.module}/../../dummy_lambda"
   output_path = "${path.module}/../../dummy_lambda/${local.app_prefix}.zip"
+
+  source {
+    filename = basename(local.source_files[0])
+    content  = data.template_file.t_file[0].rendered
+  }
+
+  source {
+    filename = basename(local.source_files[1])
+    content  = data.template_file.t_file[1].rendered
+  }
+
 }
