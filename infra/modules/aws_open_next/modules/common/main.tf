8c92fa8045c0693bf1ee3c48bac6edd689c92cb1
uolter <walter.traspadini@pagopa.it>
2025-05-29 12:52:22 +0200

    fix data archive

--- infra/modules/aws_open_next/modules/common/main.tf
+++ infra/modules/aws_open_next/modules/common/main.tf
@@ -13,10 +13,12 @@ resource "aws_s3_bucket_public_access_block" "lambda_code" {
   ignore_public_acls      = true
 }
 
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "lambda_code" {
   bucket = aws_s3_bucket.lambda_code.bucket
 
   rule {
+
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
@@ -38,5 +40,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "lambda_code" {
       days_after_initiation = 1
     }
   }
+
 
 }
