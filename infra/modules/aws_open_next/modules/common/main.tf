b961788739e4155fcdcd9ccbd7784429567d8493
uolter <walter.traspadini@pagopa.it>
2025-05-29 12:47:02 +0200

    fix missed bucket policy

--- infra/modules/aws_open_next/modules/common/main.tf
+++ infra/modules/aws_open_next/modules/common/main.tf
@@ -13,16 +13,10 @@ resource "aws_s3_bucket_public_access_block" "lambda_code" {
   ignore_public_acls      = true
 }
 
-resource "aws_s3_bucket_policy" "lambda_code" {
-  bucket = aws_s3_bucket.lambda_code.bucket
-  policy = data.aws_iam_policy_document.read_lambda_code_bucket.json
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "lambda_code" {
   bucket = aws_s3_bucket.lambda_code.bucket
 
   rule {
-
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
@@ -44,6 +38,5 @@ resource "aws_s3_bucket_lifecycle_configuration" "lambda_code" {
       days_after_initiation = 1
     }
   }
-
 
 }
