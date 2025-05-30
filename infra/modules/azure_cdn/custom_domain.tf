6929c6f0cf9af3c4c9df4f03fa667fa764e961cc
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-30 14:33:53 +0200

    Update changeset and run pre-commits

--- infra/modules/azure_cdn/custom_domain.tf
+++ infra/modules/azure_cdn/custom_domain.tf
@@ -31,9 +31,9 @@ resource "azurerm_cdn_frontdoor_custom_domain" "this" {
   dynamic "tls" {
     for_each = lookup(local.is_apex, each.value.host_name, false) ? [1] : []
     content {
-      certificate_type    = "CustomerCertificate"
+      certificate_type        = "CustomerCertificate"
       cdn_frontdoor_secret_id = azurerm_cdn_frontdoor_secret.certificate[each.key].id
-      minimum_tls_version = "TLS12"
+      minimum_tls_version     = "TLS12"
     }
   }
 }
@@ -51,14 +51,14 @@ module "rbac" {
   source  = "pagopa-dx/azure-role-assignments/azurerm"
   version = "~> 1.0"
 
-  principal_id = data.azuread_service_principal.frontdoor.object_id
+  principal_id    = data.azuread_service_principal.frontdoor.object_id
   subscription_id = data.azurerm_client_config.current.subscription_id
 
   key_vault = [{
     name                = each.value.custom_certificate.key_vault_name
     resource_group_name = each.value.custom_certificate.key_vault_resource_group_name
     has_rbac_support    = each.value.custom_certificate.key_vault_has_rbac_support
-    description = "Allow FrontDoor ${azurerm_cdn_frontdoor_endpoint.this.name} to access the certificate in the key vault"
+    description         = "Allow FrontDoor ${azurerm_cdn_frontdoor_endpoint.this.name} to access the certificate in the key vault"
 
     roles = {
       secrets = "read"
