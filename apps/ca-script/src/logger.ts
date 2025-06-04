2b08fb236f927f6188d67f9252ea492d1c6274f6
Krusty93 <andrea.grillo@pagopa.it>
2025-06-04 13:51:33 +0200

    remove debug info

--- apps/ca-script/src/logger.ts
+++
@@ -1,7 +0,0 @@
-import * as winston from "winston";
-
-export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
-});
