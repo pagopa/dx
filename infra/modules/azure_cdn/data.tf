1edfc3c0249518b4b05c30b2d77eec5eab22abea
Christian Calabrese <christian.calabrese@pagopa.it>
2025-06-06 17:24:44 +0200

    Manage custom certificate authentication via managed identity

--- infra/modules/azure_cdn/data.tf
+++ infra/modules/azure_cdn/data.tf
@@ -1,5 +1,1 @@
 data "azurerm_client_config" "current" {}
-
-data "azuread_service_principal" "frontdoor" {
-  display_name = "Microsoft.AzurefrontDoor-Cdn"
-}
--- infra/modules/azure_cdn/locals.tf
+++ infra/modules/azure_cdn/locals.tf
@@ -12,4 +12,18 @@ locals {
   is_apex = {
     for cd in var.custom_domains : cd.host_name => (cd.host_name == cd.dns.zone_name)
   }
+
+  # Create a map of unique key vaults with RBAC support using grouping
+  unique_key_vaults_rbac = {
+    for custom_domain in var.custom_domains :
+    "${custom_domain.custom_certificate.key_vault_name}:${custom_domain.custom_certificate.key_vault_resource_group_name}" => custom_domain...
+    if lookup(local.is_apex, custom_domain.host_name, false) && custom_domain.custom_certificate.key_vault_has_rbac_support
+  }
+
+  # Create a map of unique key vaults without RBAC support using grouping
+  unique_key_vaults_no_rbac = {
+    for custom_domain in var.custom_domains :
+    "${custom_domain.custom_certificate.key_vault_name}:${custom_domain.custom_certificate.key_vault_resource_group_name}" => custom_domain...
+    if lookup(local.is_apex, custom_domain.host_name, false) && !custom_domain.custom_certificate.key_vault_has_rbac_support
+  }
 }
