23bc78e13aaf6adbce555995aa0e8e095cb9e46e
uolter <walter.traspadini@pagopa.it>
2025-06-22 18:01:30 +0200

    update cloudfront module outputs

--- infra/modules/aws_open_next/modules/cloudfront/outputs.tf
+++ infra/modules/aws_open_next/modules/cloudfront/outputs.tf
@@ -19,7 +19,7 @@ output "domain_name" {
 }
 
 
-output "zone_id" {
+output "hosted_zone_id" {
   value       = aws_cloudfront_distribution.distribution.hosted_zone_id
   description = "The hosted zone ID of the CloudFront distribution."
 }
