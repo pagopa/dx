954f3ccf205c544150ee17ceec1afc3698efc890
Marco Comi <marco.comi@pagopa.it>
2025-06-23 16:42:11 +0200

    Add doctor command

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,10 +1,20 @@
 import { Command } from "commander";
 
+import { makeConsoleWriter } from "./adapters/console/index.js";
+import { doctor } from "./commands/index.js";
+import { Dependencies } from "./domain/dependencies.js";
+
+const deps: Dependencies = {
+  writer: makeConsoleWriter(),
+};
+
 const program = new Command();
 
 program
   .name("DX-CLI")
   .description("The CLI for DX-Platform")
   .version(__CLI_VERSION__);
+
+program.addCommand(doctor(deps));
 
 program.parse();
