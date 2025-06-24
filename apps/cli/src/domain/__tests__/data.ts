59b0983ad4d6d3102c64177e445f63a253eb469d
Marco Comi <marco.comi@pagopa.it>
2025-06-24 16:08:59 +0200

    Fix import

--- apps/cli/src/domain/__tests__/data.ts
+++ apps/cli/src/domain/__tests__/data.ts
@@ -1,7 +1,7 @@
 import { mock } from "vitest-mock-extended";
 
 import { NodeReader } from "../node.js";
-import { RepositoryReader } from "../reporisoty";
+import { RepositoryReader } from "../reporisoty.js";
 import { Writer } from "../writer.js";
 
 export const makeMockDependencies = () => ({
