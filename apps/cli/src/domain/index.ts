76e6ea27473a87b2a320b7f7e7224c79f8e8c9fe
Marco Comi <marco.comi@pagopa.it>
2025-06-24 18:16:02 +0200

    Remove redundant return type

--- apps/cli/src/domain/index.ts
+++ apps/cli/src/domain/index.ts
@@ -2,7 +2,7 @@ import { Dependencies } from "./dependencies.js";
 
 export const unwrapOrLogError =
   ({ logger }: Pick<Dependencies, "logger">) =>
-  async <T>(fn: () => Promise<T>): Promise<T> => {
+  async <T>(fn: () => Promise<T>) => {
     try {
       return await fn();
     } catch (error) {
