a3ab57b72917772190dec9cb9029473caa84ee31
Marco Comi <9998393+kin0992@users.noreply.github.com>
2025-06-24 16:13:43 +0200

    Create shiny-doors-own.md

--- apps/cli/src/adapters/yarn/index.ts
+++ apps/cli/src/adapters/yarn/index.ts
@@ -1,6 +1,5 @@
 import { execa } from "execa";
 import * as process from "node:process";
-import { z } from "zod";
 
 import { NodeReader, ScriptSchema } from "../../domain/node.js";
 
@@ -12,8 +11,6 @@ export const makeNodeReader = (): NodeReader => ({
 
     return stdout
       .split("\n")
-      .filter(Boolean)
-      .map((line) => ScriptSchema.parse(JSON.parse(line)))
-      .filter((item): item is z.infer<typeof ScriptSchema> => item !== null);
+      .map((line) => ScriptSchema.parse(JSON.parse(line)));
   },
 });
