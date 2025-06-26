f2d039a1b994f39f8066922cdd75241b50777a9d
Marco Comi <marco.comi@pagopa.it>
2025-06-26 11:49:47 +0200

    Fix import issues

--- apps/cli/src/adapters/commander/commands/__tests__/doctor.test.ts
+++ apps/cli/src/adapters/commander/commands/__tests__/doctor.test.ts
@@ -1,13 +1,13 @@
 import { describe, expect, it } from "vitest";
 
-import { makeMockDependencies } from "../../../../domain/__tests__/data";
-import { makeDoctorCommand } from "../doctor";
+import { makeMockDependencies } from "../../../../domain/__tests__/data.js";
+import { makeDoctorCommand } from "../doctor.js";
 
 describe("doctor command", () => {
   it("should log no repository root found", async () => {
     const deps = makeMockDependencies();
 
-    deps.repositoryReader.findRepositoryRoot.mockImplementationOnce(null);
+    deps.repositoryReader.findRepositoryRoot.mockImplementationOnce(() => null);
 
     const doctorCommand = makeDoctorCommand(deps);
     doctorCommand.parse([]);
