75be6f989e83a7033dbeed3023c77000a67ad117
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-26 12:17:24 +0000

    Revert to CommonJS output

--- actions/pr-comment/tsup.config.ts
+++ actions/pr-comment/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["cjs"],
   dts: false,
   sourcemap: false,
   clean: true,
