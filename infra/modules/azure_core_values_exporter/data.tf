ff05f54b3d647b9f3efca995b027fb07ff32675a
Christian Calabrese <christian.calabrese@pagopa.it>
2025-06-26 15:24:04 +0200

    First implementation

---
+++ infra/modules/azure_core_values_exporter/data.tf
@@ -0,0 +1,12 @@
+data "terraform_remote_state" "core" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = var.core_state.resource_group_name
+    storage_account_name = var.core_state.storage_account_name
+    container_name       = var.core_state.container_name
+    key                  = var.core_state.key
+  }
+}
+
+data "azurerm_client_config" "current" {}
