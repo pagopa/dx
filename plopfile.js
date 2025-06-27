c20b57fb5192eef84724ceafdac5684e2f773be6
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-27 08:52:50 +0000

    Update plopfile to use TypeScript generator for package creation

--- plopfile.js
+++ plopfile.js
@@ -2,9 +2,9 @@
  * Root-level plopfile for the DX monorepo
  *
  * This file allows running `yarn plop` from the workspace root
- * to generate new packages using the @pagopa/plop-generator.
+ * to generate new packages using the @pagopa/typescript-generator.
  */
 
-const plopGenerator = require("./packages/plop-generator/dist/index.cjs");
+const typescriptGenerator = require("./packages/typescript-generator/dist/index.cjs");
 
-module.exports = plopGenerator;
+module.exports = typescriptGenerator;
