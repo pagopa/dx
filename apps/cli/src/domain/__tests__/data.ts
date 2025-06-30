052019288e012324461e9244eb80dc7d5c259601
Marco Comi <marco.comi@pagopa.it>
2025-06-30 12:23:57 +0200

    Introduce logtape logger

--- apps/cli/src/domain/__tests__/data.ts
+++ apps/cli/src/domain/__tests__/data.ts
@@ -1,11 +1,9 @@
 import { mock } from "vitest-mock-extended";
 
-import { Logger } from "../logger.js";
 import { PackageJsonReader } from "../package-json.js";
 import { RepositoryReader } from "../repository.js";
 
 export const makeMockDependencies = () => ({
-  logger: mock<Logger>(),
   packageJsonReader: mock<PackageJsonReader>(),
   repositoryReader: mock<RepositoryReader>(),
 });
--- apps/cli/src/domain/dependencies.ts
+++ apps/cli/src/domain/dependencies.ts
@@ -1,9 +1,7 @@
-import { Logger } from "./logger.js";
 import { PackageJsonReader } from "./package-json.js";
 import { RepositoryReader } from "./repository.js";
 
 export interface Dependencies {
-  logger: Logger;
   packageJsonReader: PackageJsonReader;
   repositoryReader: RepositoryReader;
 }
