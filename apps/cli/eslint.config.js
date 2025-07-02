6a431bb8c1cbad11f3a4e3249b0ec5789094cad7
Marco Comi <marco.comi@pagopa.it>
2025-07-02 08:29:28 +0200

    Fix eslint rules

--- apps/cli/eslint.config.js
+++ apps/cli/eslint.config.js
@@ -1,11 +1,3 @@
 import lintRules from "@pagopa/eslint-config";
 
-export default [
-  ...lintRules,
-  {
-    files: ["src/adapters/console/**/*.ts"],
-    rules: {
-      "no-console": "off",
-    },
-  },
-];
+export default lintRules;
