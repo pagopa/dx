222e85e8b6bff3a4811e4465fb64837f8b632ace
Marco Comi <marco.comi@pagopa.it>
2025-07-04 10:35:53 +0200

    Move version command in its own file

--- apps/cli/src/adapters/commander/index.ts
+++ apps/cli/src/adapters/commander/index.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 
 import { Dependencies } from "../../domain/dependencies.js";
 import { makeDoctorCommand } from "./commands/doctor.js";
+import { makeVersionCommand } from "./commands/version.js";
 
 export const makeCli = (dependencies: Dependencies) => {
   const program = new Command();
@@ -12,6 +13,7 @@ export const makeCli = (dependencies: Dependencies) => {
     .version(__CLI_VERSION__);
 
   program.addCommand(makeDoctorCommand(dependencies));
+  program.addCommand(makeVersionCommand());
 
   return program;
 };
