d0d012b4532c92d1e49ee270d9fca6e54f9dc24a
Marco Comi <marco.comi@pagopa.it>
2025-07-04 13:05:33 +0200

    Lint files

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -1,26 +1,13 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+
+import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
-  title: "DX",
-  tagline: "Welcome to the DX documentation!",
-  favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
-  url: "https://pagopa.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/dx/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "pagopa", // Usually your GitHub org/user name.
-  projectName: "dx", // Usually your repo name.
-
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-
+  favicon: "img/favicon.ico",
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -29,42 +16,47 @@ const config: Config = {
     locales: ["en"],
   },
 
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "pagopa", // Usually your GitHub org/user name.
   plugins: [
     [
       "@docusaurus/plugin-client-redirects",
       {
-        redirects: [
-
-        ],
+        redirects: [],
       },
     ],
     [
-      '@easyops-cn/docusaurus-search-local',
+      "@easyops-cn/docusaurus-search-local",
       {
-        hashed: true,
-        language: ["en"],
-        indexDocs: true,
-        indexBlog: true,
-        docsRouteBasePath: "/docs",
         blogRouteBasePath: "/blog",
+        docsRouteBasePath: "/docs",
+        hashed: true,
+        indexBlog: true,
+        indexDocs: true,
+        language: ["en"],
       },
     ],
   ],
+
   presets: [
     [
       "classic",
       {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          sidebarCollapsible: true,
-          sidebarCollapsed: true,
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/pagopa/dx/tree/main/website/",
-        },
         blog: {
-          showReadingTime: true,
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/pagopa/dx/tree/main/website/",
+          showReadingTime: true,
+        },
+        docs: {
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/pagopa/dx/tree/main/website/",
+          sidebarCollapsed: true,
+          sidebarCollapsible: true,
+          sidebarPath: "./sidebars.ts",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -72,6 +64,9 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
+  projectName: "dx", // Usually your repo name.
+
+  tagline: "Welcome to the DX documentation!",
 
   themeConfig: {
     colorMode: {
@@ -79,14 +74,33 @@ const config: Config = {
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
+    footer: {
+      links: [
+        {
+          items: [
+            {
+              label: "Start here",
+              to: "/docs/",
+            },
+          ],
+          title: "Docs",
+        },
+        {
+          items: [
+            {
+              label: "Read announcements",
+              to: "/blog/",
+            },
+          ],
+          title: "Blog",
+        },
+      ],
+      style: "dark",
+      // copyright: `Copyright © ${new Date().getFullYear()} PagoPA. Built with Docusaurus.`,
+    },
     // Replace with your project's social card
     image: "img/pagopa-logo.png",
     navbar: {
-      title: "DX",
-      logo: {
-        alt: "DX Logo",
-        src: "img/pagopa-logo.png",
-      },
       items: [
         // {
         //   type: "docSidebar",
@@ -101,37 +115,22 @@ const config: Config = {
           position: "right",
         },
       ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Start here",
-              to: "/docs/",
-            },
-          ],
-        },
-        {
-          title: "Blog",
-          items: [
-            {
-              label: "Read announcements",
-              to: "/blog/",
-            },
-          ],
-        },
-      ],
-      // copyright: `Copyright © ${new Date().getFullYear()} PagoPA. Built with Docusaurus.`,
+      logo: {
+        alt: "DX Logo",
+        src: "img/pagopa-logo.png",
+      },
+      title: "DX",
     },
     prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
       additionalLanguages: ["hcl", "bash"],
+      darkTheme: prismThemes.dracula,
+      theme: prismThemes.github,
     },
   } satisfies Preset.ThemeConfig,
+  title: "DX",
+
+  // Set the production url of your site here
+  url: "https://pagopa.github.io",
 };
 
 export default config;
