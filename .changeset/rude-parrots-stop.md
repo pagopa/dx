6678a2b0f24c98f61272334e75318f97c65ace7e
Christian Calabrese <christian.calabrese@pagopa.it>
2025-07-04 15:55:38 +0200

    Fix bug of missing storage_account_id when assigning roles on specific blob containers, tables or queues

---
+++ .changeset/rude-parrots-stop.md
@@ -0,0 +1,5 @@
+---
+"azure_role_assignments": patch
+---
+
+Fix bug of missing storage_account_id when assigning roles on specific blob containers, tables or queues
--- infra/modules/azure_role_assignments/modules/storage_account/locals.tf
+++ infra/modules/azure_role_assignments/modules/storage_account/locals.tf
@@ -5,7 +5,7 @@ locals {
       storage_account_id            = "/subscriptions/${var.subscription_id}/resourceGroups/${blob.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${blob.storage_account_name}"
       resource_group_name           = blob.resource_group_name
       container_name                = blob.container_name
-      container_resource_manager_id = blob.container_name != "*" ? "${blob.storage_account_id}/blobServices/default/containers/${blob.container_name}" : "*"
+      container_resource_manager_id = blob.container_name != "*" ? "/subscriptions/${var.subscription_id}/resourceGroups/${blob.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${blob.storage_account_name}/blobServices/default/containers/${blob.container_name}" : "*"
       role                          = blob.role
       description                   = blob.description
     }
@@ -19,7 +19,7 @@ locals {
       storage_account_id        = "/subscriptions/${var.subscription_id}/resourceGroups/${table.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${table.storage_account_name}"
       resource_group_name       = table.resource_group_name
       table_name                = table.table_name
-      table_resource_manager_id = table.table_name != "*" ? "${table.storage_account_id}/tableServices/default/tables/${table.table_name}" : "*"
+      table_resource_manager_id = table.table_name != "*" ? "/subscriptions/${var.subscription_id}/resourceGroups/${table.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${table.storage_account_name}/tableServices/default/tables/${table.table_name}" : "*"
       role                      = table.role
       description               = table.description
     }
@@ -33,7 +33,7 @@ locals {
       storage_account_id        = "/subscriptions/${var.subscription_id}/resourceGroups/${queue.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${queue.storage_account_name}"
       resource_group_name       = queue.resource_group_name
       queue_name                = queue.queue_name
-      queue_resource_manager_id = queue.queue_name != "*" ? "${queue.storage_account_id}/queueServices/default/queues/${queue.queue_name}" : "*"
+      queue_resource_manager_id = queue.queue_name != "*" ? "/subscriptions/${var.subscription_id}/resourceGroups/${queue.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${queue.storage_account_name}/queueServices/default/queues/${queue.queue_name}" : "*"
       role                      = queue.role
       description               = queue.description
     }
