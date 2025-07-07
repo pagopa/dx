51628a49d707e6d32ac1fc3c74c3029e4cc97fc1
Christian Calabrese <christian.calabrese@pagopa.it>
2025-07-07 15:15:01 +0200

    Add dx provider to appi module in azure_core_infra

---
+++ infra/modules/azure_core_infra/_modules/application_insights/appi.tf
@@ -0,0 +1,26 @@
+resource "azurerm_application_insights" "appi" {
+  name = provider::dx::resource_name(merge(
+    var.naming_config,
+    {
+      name          = "common",
+      resource_type = "application_insights",
+  }))
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  disable_ip_masking   = true
+  application_type     = "other"
+  daily_data_cap_in_gb = var.daily_data_cap_in_gb
+
+  workspace_id = var.log_analytics_workspace_id
+
+  tags = var.tags
+}
+
+#tfsec:ignore:AZU023
+resource "azurerm_key_vault_secret" "appinsights_instrumentation_key" {
+  name         = "appinsights-instrumentation-key"
+  value        = azurerm_application_insights.appi.instrumentation_key
+  content_type = "Application insights instrumentation key for ${azurerm_application_insights.appi.name}"
+
+  key_vault_id = var.key_vault_id
+}
