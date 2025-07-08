88085d2384b91bb6b6c2ace6875d2a736f4e53df
Marco Comi <marco.comi@pagopa.it>
2025-07-08 13:44:38 +0200

    Do remove count

--- infra/bootstrapper/_modules/bootstrapper/main.tf
+++ infra/bootstrapper/_modules/bootstrapper/main.tf
@@ -106,7 +106,6 @@ resource "azurerm_role_assignment" "contributor" {
 }
 
 resource "github_actions_secret" "codecov_token" {
-  count           = var.environment.env_short == "p" ? 1 : 0
   repository      = var.repository.name
   secret_name     = "CODECOV_TOKEN"
   plaintext_value = data.azurerm_key_vault_secret.codecov_token.value
