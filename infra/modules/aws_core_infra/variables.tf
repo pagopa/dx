feccfda5b42d387079aef9f5599ec0955c1f5ad5
Christian Calabrese <christian.calabrese@pagopa.it>
2025-07-15 17:33:34 +0200

    First commit

---
+++ infra/modules/aws_core_infra/variables.tf
@@ -0,0 +1,16 @@
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to assign to the resources."
+}
+
+variable "environment" {
+  type = object({
+    prefix          = string
+    env_short       = string
+    location        = string
+    domain          = string
+    instance_number = string
+  })
+
+  description = "Values which are used to generate resource names and location short names. They are all mandatory except for domain, which should not be used only in the case of a resource used by multiple domains."
+}
