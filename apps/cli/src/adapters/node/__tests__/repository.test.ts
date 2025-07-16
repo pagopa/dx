9eacdc6d52d24bca9c2609e5dd88480661ed4bd7
Marco Comi <marco.comi@pagopa.it>
2025-07-16 18:35:23 +0200

    Fix implementation according to the unit tests

--- apps/cli/src/adapters/node/__tests__/repository.test.ts
+++ apps/cli/src/adapters/node/__tests__/repository.test.ts
@@ -2,7 +2,16 @@ import { err, ok } from "neverthrow";
 import fs from "node:fs/promises";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
+import { PackageJson } from "../../../domain/package-json.js";
 import { makeRepositoryReader } from "../repository.js";
+import { makeMockPackageJson, makeMockPnpmWorkspaceYaml } from "./data.js";
+
+async function* makeAsyncPaths(paths: string[]) {
+  for (const path of paths) {
+    await new Promise((resolve) => resolve(path));
+    yield path;
+  }
+}
 
 describe("makeRepositoryReader", () => {
   beforeEach(() => {
@@ -52,5 +61,85 @@ describe("makeRepositoryReader", () => {
       );
       expect(fs.glob).toHaveBeenCalledTimes(0);
     });
+  });
+
+  it("should return the workspaces", async () => {
+    vi.spyOn(fs, "readFile")
+      // First read the pnpm-workspace.yaml file
+      .mockResolvedValueOnce(makeMockPnpmWorkspaceYaml())
+      // Then read the package.json files
+      .mockResolvedValueOnce(
+        JSON.stringify(
+          makeMockPackageJson({ name: "foo" as PackageJson["name"] }),
+        ),
+      )
+      .mockResolvedValueOnce(
+        JSON.stringify(
+          makeMockPackageJson({ name: "bar" as PackageJson["name"] }),
+        ),
+      );
+    vi.spyOn(fs, "glob").mockImplementationOnce(() =>
+      makeAsyncPaths(["packages/foo", "packages/bar"]),
+    );
+
+    const repositoryReader = makeRepositoryReader();
+    const result = await repositoryReader.getWorkspaces(repoRoot);
+
+    expect(result).toStrictEqual(
+      ok([
+        {
+          name: "foo",
+          path: "/some/repo/root/packages/foo",
+        },
+        {
+          name: "bar",
+          path: "/some/repo/root/packages/bar",
+        },
+      ]),
+    );
+    expect(fs.readFile).nthCalledWith(
+      1,
+      "/some/repo/root/pnpm-workspace.yaml",
+      "utf-8",
+    );
+    expect(fs.glob).nthCalledWith(1, "packages/*", { cwd: repoRoot });
+    expect(fs.readFile).nthCalledWith(
+      2,
+      "/some/repo/root/packages/foo/package.json",
+      "utf-8",
+    );
+    expect(fs.readFile).nthCalledWith(
+      3,
+      "/some/repo/root/packages/bar/package.json",
+      "utf-8",
+    );
+  });
+
+  it("should return an error when the glob function fails", async () => {
+    vi.spyOn(fs, "readFile")
+      // First read the pnpm-workspace.yaml file
+      .mockResolvedValueOnce(makeMockPnpmWorkspaceYaml());
+    vi.spyOn(fs, "glob").mockImplementationOnce(() => ({
+      async next() {
+        throw new Error("glob failed");
+      },
+      [Symbol.asyncIterator]() {
+        return this;
+      },
+    }));
+
+    const repositoryReader = makeRepositoryReader();
+    const result = await repositoryReader.getWorkspaces(repoRoot);
+
+    expect(result).toStrictEqual(
+      err(new Error("Failed to resolve workspace glob: packages/*")),
+    );
+    expect(fs.readFile).nthCalledWith(
+      1,
+      "/some/repo/root/pnpm-workspace.yaml",
+      "utf-8",
+    );
+    expect(fs.glob).nthCalledWith(1, "packages/*", { cwd: repoRoot });
+    expect(fs.readFile).toHaveBeenCalledTimes(1);
   });
 });
