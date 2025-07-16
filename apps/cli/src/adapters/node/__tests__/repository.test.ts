847047ccc5c9a797dca776486070376e3e005069
Marco Comi <marco.comi@pagopa.it>
2025-07-16 18:35:07 +0200

    Update unit tests

--- apps/cli/src/adapters/node/__tests__/repository.test.ts
+++ apps/cli/src/adapters/node/__tests__/repository.test.ts
@@ -1,4 +1,4 @@
-import { err } from "neverthrow";
+import { err, ok } from "neverthrow";
 import fs from "node:fs/promises";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
@@ -25,6 +25,32 @@ describe("makeRepositoryReader", () => {
           new Error("Failed to read file: /some/repo/root/pnpm-workspace.yaml"),
         ),
       );
+    });
+    it("should return an error when pnpm-workspace.yaml is malformed", async () => {
+      vi.spyOn(fs, "readFile").mockResolvedValueOnce(
+        "invalid: content: not a valid YAML",
+      );
+
+      const repositoryReader = makeRepositoryReader();
+      const result = await repositoryReader.getWorkspaces(repoRoot);
+
+      expect(result).toStrictEqual(err(new Error("Failed to parse YAML")));
+    });
+
+    it("should return no workspaces if the packages entry is not in the pnpm-workspace.yaml file", async () => {
+      vi.spyOn(fs, "readFile").mockResolvedValueOnce("aValidYaml");
+      vi.spyOn(fs, "glob");
+
+      const repositoryReader = makeRepositoryReader();
+      const result = await repositoryReader.getWorkspaces(repoRoot);
+
+      expect(result).toStrictEqual(ok([]));
+      expect(fs.readFile).nthCalledWith(
+        1,
+        "/some/repo/root/pnpm-workspace.yaml",
+        "utf-8",
+      );
+      expect(fs.glob).toHaveBeenCalledTimes(0);
     });
   });
 });
