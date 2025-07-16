ded234fcf2742c6204186e0bfe6c947f6767296e
Marco Comi <marco.comi@pagopa.it>
2025-07-16 18:13:24 +0200

    Add implementation to check the workspaces

--- apps/cli/src/adapters/node/repository.ts
+++ apps/cli/src/adapters/node/repository.ts
@@ -1,9 +1,13 @@
 import { ResultAsync } from "neverthrow";
 import { join } from "node:path";
+import { z } from "zod/v4";
 
+import { packageJsonSchema } from "../../domain/package-json.js";
 import { RepositoryReader } from "../../domain/repository.js";
-import { Workspace } from "../../domain/workspace.js";
-import { fileExists, readFile } from "./fs/file-reader.js";
+import { Workspace, workspaceSchema } from "../../domain/workspace.js";
+import { parseYaml } from "../yaml/index.js";
+import { decode } from "../zod/index.js";
+import { fileExists, readFile, readFileAndDecode } from "./fs/file-reader.js";
 
 const findRepositoryRoot = (
   dir = process.cwd(),
@@ -19,13 +23,43 @@ const findRepositoryRoot = (
     .map(() => dir);
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getWorkspaces = (_repoRoot: string): ResultAsync<Workspace[], Error> =>
-  // TODO: Implement this. For now, we're returning an empty array.
+const resolveWorkspacePattern = (repoRoot: string) => (pattern: string) =>
   ResultAsync.fromPromise(
-    Promise.resolve([]),
-    () => new Error("Failed to get workspaces"),
+    Array.fromAsync(fs.glob(pattern, { cwd: repoRoot })),
+    (cause) =>
+      new Error(`Failed to resolve workspace glob: ${pattern}`, {
+        cause,
+      }),
+  ).map((subDirectories) =>
+    // Create the absolute path to the subdirectory
+    subDirectories.map((directory) => join(repoRoot, directory)),
   );
+
+const getWorkspaces = (repoRoot: string): ResultAsync<Workspace[], Error> =>
+  readFile(join(repoRoot, "pnpm-workspace.yaml"))
+    .andThen(parseYaml)
+    // Decode the pnpm-workspace.yaml file to a zod schema
+    .andThen(decode(z.object({ packages: z.array(z.string()) })))
+    .andThen(({ packages }) =>
+      // For every package pattern in the pnpm-workspace.yaml file, get the list of subdirectories
+      ResultAsync.combine(packages.map(resolveWorkspacePattern(repoRoot)))
+        .map((workspacesList) => workspacesList.flat())
+        .andThen((workspaceFolders) => {
+          // For every subdirectory, read the package.json file and decode it to a zod schema
+          const workspaceResults = workspaceFolders.map(
+            (nodeWorkspaceDirectory) =>
+              readFileAndDecode(
+                join(nodeWorkspaceDirectory, "package.json"),
+                packageJsonSchema,
+              ).map(({ name }) =>
+                // Create the workspace object using the package.json name and the nodeWorkspaceDirectory
+                workspaceSchema.parse({ name, path: nodeWorkspaceDirectory }),
+              ),
+          );
+          // Execute and combine the results
+          return ResultAsync.combine(workspaceResults);
+        }),
+    );
 
 export const makeRepositoryReader = (): RepositoryReader => ({
   fileExists,
