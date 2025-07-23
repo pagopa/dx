5b1ea1d437ae14125a499093295619a57517d9f7
Marco Comi <marco.comi@pagopa.it>
2025-07-23 12:05:27 +0200

    Add error message

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,5 +1,5 @@
 import "core-js/actual/set/index.js";
-import { configure, getConsoleSink } from "@logtape/logtape";
+import { configure, getConsoleSink, getLogger } from "@logtape/logtape";
 
 import { makeCli } from "./adapters/commander/index.js";
 import { makeValidationReporter } from "./adapters/logtape/validation-reporter.js";
@@ -20,23 +20,27 @@ await configure({
   sinks: { console: getConsoleSink() },
 });
 
+const logger = getLogger(["dx-cli"]);
+// Creating the adapters
 const repositoryReader = makeRepositoryReader();
 const packageJsonReader = makePackageJsonReader();
+const validationReporter = makeValidationReporter();
 
+// Read once the package.json file in the repo root
 const repoPackageJson = await repositoryReader
   .findRepositoryRoot(process.cwd())
   .andThen(packageJsonReader.readPackageJson);
 
 if (repoPackageJson.isErr()) {
+  logger.error("Repository does not contain a package.json file");
   process.exit(1);
 }
 const packageJson = repoPackageJson.value;
-
 const deps: Dependencies = {
   packageJson,
   packageJsonReader,
   repositoryReader,
-  validationReporter: makeValidationReporter(),
+  validationReporter,
 };
 
 const config = getConfig();
