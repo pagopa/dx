e48cdacd18d647a1bb9c316535f01d25b4b7995a
Christian Calabrese <christian.calabrese@pagopa.it>
2025-07-25 17:03:48 +0200

    Remove evidence of vpn

--- infra/modules/aws_core_infra/examples/basic/main.tf
+++ infra/modules/aws_core_infra/examples/basic/main.tf
@@ -7,8 +7,7 @@ module "core" {
   vpc_cidr = "10.1.0.0/16"
 
   # Cost-optimized configuration
-  nat_gateway_count = 0     # Disable NAT gateways for testing
-  vpn_enabled       = false # Optional for basic setup
+  nat_gateway_count = 0 # Disable NAT gateways for testing
 
   tags = local.tags
 }
