c1da66026e49911e5dd68cb61452088de9e18c7e
Christian Calabrese <christian.calabrese@pagopa.it>
2025-08-05 18:15:17 +0200

    Clarify that the thumprint list is not needed
    Co-authored-by: Mario Mupo <43968294+mamu0@users.noreply.github.com>

--- infra/modules/aws_github_environment_bootstrap/oidc.tf
+++ infra/modules/aws_github_environment_bootstrap/oidc.tf
@@ -5,7 +5,8 @@ resource "aws_iam_openid_connect_provider" "this" {
     "sts.amazonaws.com",
   ]
 
-  # The thumbprint list is not required for OIDC providers like GitHub
+  # The thumbprint list is not required for OIDC providers like GitHub, so a placeholder is enough
+  # Ref. https://medium.com/%40thiagosalvatore/using-terraform-to-connect-github-actions-and-aws-with-oidc-0e3d27f00123
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_openid_connect_provider#argument-reference
   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
