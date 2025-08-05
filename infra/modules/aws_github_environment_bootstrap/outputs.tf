c661303e8d197e9e71d6f4d6af41e6b000307382
Christian Calabrese <christian.calabrese@pagopa.it>
2025-08-05 18:29:56 +0200

    Change id into arn in outputs

--- infra/modules/aws_github_environment_bootstrap/outputs.tf
+++ infra/modules/aws_github_environment_bootstrap/outputs.tf
@@ -10,21 +10,21 @@ output "identities" {
   value = {
     app = {
       ci = {
-        id   = aws_iam_role.app_ci.arn
+        arn  = aws_iam_role.app_ci.arn
         name = aws_iam_role.app_ci.name
       }
       cd = {
-        id   = aws_iam_role.app_cd.arn
+        arn  = aws_iam_role.app_cd.arn
         name = aws_iam_role.app_cd.name
       }
     }
     infra = {
       ci = {
-        id   = aws_iam_role.infra_ci.arn
+        arn  = aws_iam_role.infra_ci.arn
         name = aws_iam_role.infra_ci.name
       }
       cd = {
-        id   = aws_iam_role.infra_cd.arn
+        arn  = aws_iam_role.infra_cd.arn
         name = aws_iam_role.infra_cd.name
       }
     }
