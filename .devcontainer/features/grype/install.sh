11ef19c4a6c00ae16a44b5e4267f01ba837c6731
Mario Mupo <mario.mupo@pagopa.it>
2025-08-25 17:57:30 +0200

    feat: used existent features and removed sboms folder

--- .devcontainer/features/grype/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-curl -sSfL https://get.anchore.io/grype | sudo sh -s -- -b /usr/local/bin
-
-echo "Grype installed successfully!"
--- .devcontainer/features/syft/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-curl -sSfL https://get.anchore.io/syft | sudo sh -s -- -b /usr/local/bin
-
-echo "Syft installed successfully!"
