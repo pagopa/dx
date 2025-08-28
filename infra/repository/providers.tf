9443df2db4f61484471c64061274fa01f59e1b45
Christian Calabrese <christian.calabrese@pagopa.it>
2025-08-28 17:50:22 +0200

    Complete upgrade of bootstrap to v3

---
+++ infra/repository/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "dx-d-itn-tfstate-rg-01"
+    storage_account_name = "dxditntfstatest01"
+    container_name       = "terraform-state"
+    key                  = "dx.repository.bootstrapper.dev.tfstate"
+  }
+}
+
+# GitHub provider configuration
+provider "github" {
+  owner = "pagopa"
+}
