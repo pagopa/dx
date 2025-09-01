ab384713c7887d059c0a67d2ddf69e96a1ae4580
Christian Calabrese <christian.calabrese@pagopa.it>
2025-09-01 14:59:08 +0200

    Changed repository configuration state file name

--- infra/repository/providers.tf
+++ infra/repository/providers.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "dx-d-itn-tfstate-rg-01"
     storage_account_name = "dxditntfstatest01"
     container_name       = "terraform-state"
-    key                  = "dx.repository.bootstrapper.dev.tfstate"
+    key                  = "dx.repository.tfstate"
   }
 }
 
