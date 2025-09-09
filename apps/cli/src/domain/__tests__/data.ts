459637c1519c57ae335f77e8338aab05cefba64b
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-09-09 10:54:05 +0000

    Fix typecheck

--- apps/cli/src/domain/__tests__/data.ts
+++ apps/cli/src/domain/__tests__/data.ts
@@ -1,3 +1,4 @@
+import { vi } from "vitest";
 import { mock } from "vitest-mock-extended";
 
 import { Config } from "../../config.js";
@@ -27,6 +28,8 @@ export const makeMockPackageJson = (
 };
 
 export const makeMockDependencies = () => ({
+  applyCodemod: vi.fn(),
+  listCodemods: vi.fn(),
   packageJson: makeMockPackageJson(),
   packageJsonReader: mock<PackageJsonReader>(),
   repositoryReader: mock<RepositoryReader>(),
