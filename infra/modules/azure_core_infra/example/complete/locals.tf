068e8a421cd28a79934f6bdd88c5a713edf9b108
Mario Mupo <mario.mupo@pagopa.it>
2025-09-11 12:12:53 +0200

    fix: test

--- infra/modules/azure_core_infra/example/complete/locals.tf
+++ infra/modules/azure_core_infra/example/complete/locals.tf
@@ -19,9 +19,6 @@ locals {
     # FOR TESTING PURPOSES ONLY - TO BE REMOVED
     CertificatePem       = "-----BEGIN CERTIFICATE-----MIIDXTCCAkWg...-----END CERTIFICATE-----"
     UUID                 = "123e4567-e89b-12d3-a456-426614174000"
-    AuthToken            = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    AzureSecretKey       = "Z196UjB1bFE2TlM4YVBnZmdnUkJhPQ=="
-    hidden-link          = "https://example.com,https://example.org"
-    APPINSIGHTS_INSTRUMENTATIONKEY = "00000000-0000-0000-0000-000000000000"
+    hidden-link          = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/my-resource-group/providers/Microsoft.Sql/servers/my-database-server-name/databases/my-database-name"
   }
 }
