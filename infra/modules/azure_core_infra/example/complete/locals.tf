7cf1139d8bf887c47e1f7fa9800a0b3132a2a0c4
Mario Mupo <mario.mupo@pagopa.it>
2025-09-11 16:43:41 +0200

    feat: refactor generic regex

--- infra/modules/azure_core_infra/example/complete/locals.tf
+++ infra/modules/azure_core_infra/example/complete/locals.tf
@@ -17,8 +17,6 @@ locals {
     Source         = "https://github.com/pagopa/dx/modules/azure_core_infra/examples/complete"
 
     # FOR TESTING PURPOSES ONLY - TO BE REMOVED
-    CertificatePem       = "-----BEGIN CERTIFICATE-----MIIDXTCCAkWg...-----END CERTIFICATE-----"
-    UUID                 = "123e4567-e89b-12d3-a456-426614174000"
-    hidden-link          = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/my-resource-group/providers/Microsoft.Sql/servers/my-database-server-name/databases/my-database-name"
+    hidden-link          = "/subscriptions/0000000-0000-0000-0000-00000/resourceGroups/my-resource-group/providers/Microsoft.Sql/servers/my-database-server-name/databases/my-database-name"
   }
 }
