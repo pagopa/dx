eb14f055f136366313f27fd25960c1722adc4dc8
Christian Calabrese <christian.calabrese@pagopa.it>
2025-09-11 10:22:57 +0200

    Rewrite VPN and dns-forwarder modules into plain resources (#844)
    Currently, the DX azure-core-infra module makes use of
    terraform-azurerm-v4 modules to create the [VPN gateway and the DNS
    forwarder](https://github.com/pagopa/dx/blob/0f1cf459d486b5406294876bbe4116e099aea88e/infra/modules/azure_core_infra/_modules/vpn/vpn.tf).
    To allow smooth new implementations and maintainability, the two modules
    have been replaced with plain resources. To respect DX naming
    convention, VPN resources will require a replacement, hence a major is
    required.
    
    Resolves CES-1247
    Depends on #843

---
+++ infra/modules/azure_core_infra/_modules/vpn/locals.tf
@@ -0,0 +1,37 @@
+locals {
+  use_cases = {
+    "default" = {
+      vpn_connections_number = 1
+    }
+    "high_availability" = {
+      vpn_connections_number = 2
+    }
+  }
+
+  aws = {
+    bgp_asn = 65000
+    # First level key is the VPN connection index, second level key is the tunnel index
+    inside_cidrs = {
+      0 = {
+        0 = "169.254.21.0/30"
+        1 = "169.254.22.0/30"
+      },
+      1 = {
+        0 = "169.254.21.4/30"
+        1 = "169.254.22.4/30"
+      }
+    }
+  }
+
+  corefile_content = <<EOF
+.:53 {
+  errors
+  ready
+  health
+  forward . 168.63.129.16
+  cache 30
+  loop
+  reload
+}
+EOF
+}
