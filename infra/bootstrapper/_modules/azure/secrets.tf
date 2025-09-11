374677ac4e63581bfea2dfa8a386428500cac754
Christian Calabrese <christian.calabrese@pagopa.it>
2025-09-11 18:07:59 +0200

    Using app-*-ci for static web apps build

--- infra/bootstrapper/_modules/azure/secrets.tf
+++ infra/bootstrapper/_modules/azure/secrets.tf
@@ -5,6 +5,14 @@ resource "github_actions_secret" "codecov_token" {
   plaintext_value = data.azurerm_key_vault_secret.codecov_token[0].value
 }
 
+resource "github_actions_environment_secret" "appi_instrumentation_key_ci" {
+  repository      = var.repository.name
+  environment     = "app-${local.env_long}-ci"
+  secret_name     = "APPLICATIONINSIGHTS_INSTRUMENTATION_KEY"
+  plaintext_value = data.azurerm_key_vault_secret.appi_instrumentation_key.value
+}
+
+
 resource "github_actions_environment_secret" "appi_instrumentation_key" {
   repository      = var.repository.name
   environment     = "app-${local.env_long}-cd"
