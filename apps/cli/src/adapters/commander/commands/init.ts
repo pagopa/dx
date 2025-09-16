7c90a5c8efbb99f28218831a73ec263654d12391
Marco Comi <marco.comi@pagopa.it>
2025-09-16 15:46:06 +0200

    First draft of the init command that runs the PagoPA monorepo generator

--- apps/cli/src/adapters/commander/commands/init.ts
+++ apps/cli/src/adapters/commander/commands/init.ts
@@ -1,4 +1,6 @@
+import scaffoldMonorepo from "@pagopa/monorepo-generator";
 import { Command } from "commander";
+import nodePlop from "node-plop";
 
 export const makeInitCommand = (): Command =>
   new Command()
@@ -7,4 +9,10 @@ export const makeInitCommand = (): Command =>
     .action(async () => {
       //eslint-disable-next-line no-console
       console.log("(WIP) - Initializing a new monorepo...");
+
+      const plop = await nodePlop();
+      scaffoldMonorepo(plop);
+
+      const generator = plop.getGenerator("monorepo");
+      await generator.runPrompts();
     });
