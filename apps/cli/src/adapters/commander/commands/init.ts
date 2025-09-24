39a191bfc5764409a8b9d397db015586c67833e9
Marco Comi <marco.comi@pagopa.it>
2025-09-24 12:20:39 +0200

    Refactor init command

--- apps/cli/src/adapters/commander/commands/init.ts
+++ apps/cli/src/adapters/commander/commands/init.ts
@@ -1,7 +1,30 @@
 import scaffoldMonorepo from "@pagopa/monorepo-generator";
 import { Command } from "commander";
 import { Result, ResultAsync } from "neverthrow";
-import nodePlop from "node-plop";
+import nodePlop, { NodePlopAPI, PlopGenerator } from "node-plop";
+
+const initPlop = () =>
+  ResultAsync.fromPromise(
+    nodePlop(),
+    () => new Error("Failed to initialize plop"),
+  );
+
+const getGenerator = (plopAPI: NodePlopAPI) =>
+  Result.fromThrowable(
+    plopAPI.getGenerator,
+    () => new Error("Generator not found"),
+  );
+
+const runGenerator = (generator: PlopGenerator) =>
+  ResultAsync.fromPromise(
+    generator.runPrompts(),
+    () => new Error("Failed to run the generator prompts"),
+  ).andThen((answers) =>
+    ResultAsync.fromPromise(
+      generator.runActions(answers),
+      () => new Error("Failed to run the generator actions"),
+    ),
+  );
 
 export const makeInitCommand = (): Command =>
   new Command()
@@ -13,27 +36,10 @@ export const makeInitCommand = (): Command =>
       new Command("project")
         .description("Initialize a new monorepo project")
         .action(async function () {
-          await ResultAsync.fromPromise(
-            nodePlop(),
-            () => new Error("Failed to initialize plop"),
-          )
+          await initPlop()
             .andTee(scaffoldMonorepo)
-            .andThen((plop) =>
-              Result.fromThrowable(
-                () => plop.getGenerator("monorepo"),
-                () => new Error("Failed to get the monorepo generator"),
-              )().asyncAndThen((generator) =>
-                ResultAsync.fromPromise(
-                  generator.runPrompts(),
-                  () => new Error("Failed to run the generator prompts"),
-                ).andThen((answers) =>
-                  ResultAsync.fromPromise(
-                    generator.runActions(answers),
-                    () => new Error("Failed to run the generator actions"),
-                  ),
-                ),
-              ),
-            )
+            .andThen((plop) => getGenerator(plop)("monorepo"))
+            .andThen(runGenerator)
             .andTee(() => {
               // eslint-disable-next-line no-console
               console.log("Monorepo initialized successfully ✅");
