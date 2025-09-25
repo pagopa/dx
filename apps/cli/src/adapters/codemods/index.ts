8e53881befdd85562495b9a496f248297af835c6
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-09-25 14:47:39 +0000

    Remove the example codemod

--- apps/cli/src/adapters/codemods/index.ts
+++ apps/cli/src/adapters/codemods/index.ts
@@ -1,4 +1,3 @@
-import example from "./example.js";
 import { LocalCodemodRegistry } from "./registry.js";
 import { updateCodeReview } from "./update-code-review.js";
 import { useAzureAppsvc } from "./use-azure-appsvc.js";
@@ -6,7 +5,6 @@ import usePnpm from "./use-pnpm.js";
 
 const registry = new LocalCodemodRegistry();
 
-registry.add(example);
 registry.add(usePnpm);
 registry.add(useAzureAppsvc);
 registry.add(updateCodeReview);
