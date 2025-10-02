ab845af6640fc157bcaa8c1038df4a392480c4ad
Andrea Grillo <andrea.grillo@pagopa.it>
2025-10-02 11:04:20 +0200

    Apply network boundaries to VNets in Dev (#956)

--- infra/resources/_modules/testing/networking.tf
+++ infra/resources/_modules/testing/networking.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "tests" {
   ))
   location            = data.azurerm_resource_group.tests[each.value].location
   resource_group_name = data.azurerm_resource_group.tests[each.value].name
-  address_space       = [format("10.%d.0.0/16", 20 + index(tolist(var.test_modes), each.key))]
+  address_space       = [local.vnet_address_space_by_mode[each.key]]
 
   tags = var.tags
 }
@@ -25,14 +25,7 @@ resource "dx_available_subnet_cidr" "pep_snet_cidrs" {
 resource "azurerm_subnet" "pep_snets" {
   for_each = var.test_modes
 
-  name = provider::dx::resource_name(
-    merge(
-      var.environment,
-      {
-        resource_type = "subnet",
-        name          = "pep",
-      }
-  ))
+  name                 = local.pep_subnet_name
   virtual_network_name = azurerm_virtual_network.tests[each.value].name
   resource_group_name  = azurerm_virtual_network.tests[each.value].resource_group_name
   address_prefixes     = [dx_available_subnet_cidr.pep_snet_cidrs[each.value].cidr_block]
@@ -41,25 +34,82 @@ resource "azurerm_subnet" "pep_snets" {
 resource "azurerm_virtual_network_peering" "common_to_tests" {
   for_each = var.test_modes
 
-  name                      = "${var.vnet_common.name}-to-${azurerm_virtual_network.tests[each.value].name}"
-  resource_group_name       = var.vnet_common.resource_group_name
-  virtual_network_name      = var.vnet_common.name
-  remote_virtual_network_id = azurerm_virtual_network.tests[each.value].id
+  name                = "${var.vnet_common.name}-to-${azurerm_virtual_network.tests[each.value].name}"
+  resource_group_name = var.vnet_common.resource_group_name
 
-  local_subnet_names = ["dx-d-itn-github-runner-snet-01"]
+  virtual_network_name = var.vnet_common.name
+  local_subnet_names   = local.common_subnets
+
+  remote_virtual_network_id = azurerm_virtual_network.tests[each.value].id
+  remote_subnet_names       = local.tests_subnets
+
+  peer_complete_virtual_networks_enabled = false
+  allow_virtual_network_access           = true
+
+  triggers = {
+    local_subnet_names  = join(",", local.common_subnets)
+    remote_subnet_names = join(",", local.tests_subnets)
+  }
 }
 
 resource "azurerm_virtual_network_peering" "tests_to_common" {
   for_each = var.test_modes
 
-  name = "${azurerm_virtual_network.tests[each.value].name}-to-${var.vnet_common.name}"
+  name                = "${azurerm_virtual_network.tests[each.value].name}-to-${var.vnet_common.name}"
+  resource_group_name = azurerm_virtual_network.tests[each.value].resource_group_name
 
-  resource_group_name       = azurerm_virtual_network.tests[each.value].resource_group_name
-  virtual_network_name      = azurerm_virtual_network.tests[each.value].name
+  virtual_network_name = azurerm_virtual_network.tests[each.value].name
+  local_subnet_names   = local.tests_subnets
+
   remote_virtual_network_id = var.vnet_common.id
+  remote_subnet_names       = local.common_subnets
 
-  allow_virtual_network_access = false
-  allow_forwarded_traffic      = false
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
+  peer_complete_virtual_networks_enabled = false
+  allow_virtual_network_access           = false
+
+  triggers = {
+    local_subnet_names  = join(",", local.tests_subnets)
+    remote_subnet_names = join(",", local.common_subnets)
+  }
+}
+
+resource "azurerm_network_security_group" "common" {
+  name = provider::dx::resource_name(
+    merge(
+      var.environment,
+      {
+        resource_type = "network_security_group",
+        name          = "common-vnet",
+      }
+  ))
+  location            = var.environment.location
+  resource_group_name = var.vnet_common.resource_group_name
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "common_runner_to_tests_pep" {
+  for_each = local.subnets_to_associate
+
+  subnet_id                 = each.key
+  network_security_group_id = azurerm_network_security_group.common.id
+}
+
+resource "azurerm_network_security_rule" "deny_common_to_tests_vnets" {
+  for_each = var.test_modes
+
+  name      = "DenyToVNet${each.value}"
+  priority  = tonumber(format("10%d", 0 + index(tolist(var.test_modes), each.key)))
+  direction = "Outbound"
+  access    = "Deny"
+
+  protocol              = "*"
+  source_port_range     = "*"
+  source_address_prefix = "*"
+
+  destination_port_range     = "*"
+  destination_address_prefix = local.vnet_address_space_by_mode[each.key]
+
+  resource_group_name         = var.vnet_common.resource_group_name
+  network_security_group_name = azurerm_network_security_group.common.name
 }
--- infra/resources/dev/tests.tf
+++ infra/resources/dev/tests.tf
@@ -8,7 +8,10 @@ module "testing" {
     name                = data.azurerm_virtual_network.common.name
     id                  = data.azurerm_virtual_network.common.id
     resource_group_name = data.azurerm_virtual_network.common.resource_group_name
+    subnet_ids          = data.azurerm_subnet.subnets[*].id
   }
+
+  runner_subnet_name = data.azurerm_subnet.runner.name
 
   private_dns_zone_names = data.azurerm_private_dns_zone.tests_peps[*].name
 
