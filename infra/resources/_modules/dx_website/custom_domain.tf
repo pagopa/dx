a63af7ddbe1a0baf8bc4841818f6050f595c8037
Christian Calabrese <christian.calabrese@pagopa.it>
2025-10-09 14:26:31 +0200

    The static web app validation record can use a placeholder after first creation

--- infra/resources/_modules/dx_website/custom_domain.tf
+++ infra/resources/_modules/dx_website/custom_domain.tf
@@ -10,7 +10,7 @@ resource "azurerm_dns_txt_record" "validation" {
   resource_group_name = var.network_resource_group_name
   ttl                 = 300
   record {
-    value = azurerm_static_web_app_custom_domain.this.validation_token
+    value = azurerm_static_site_custom_domain.this.validation_token == "" ? "validated" : azurerm_static_site_custom_domain.this.validation_token
   }
 }
 
