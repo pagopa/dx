be019485bde5cc34ed7190b08d783d7304edd256
Krusty93 <andrea.grillo@pagopa.it>
2025-10-20 14:27:16 +0200

    update span attributes

--- actions/setup-telemetry/src/post.ts
+++ actions/setup-telemetry/src/post.ts
@@ -5,9 +5,6 @@ import { trace, context as otelContext } from "@opentelemetry/api";
 async function post(): Promise<void> {
   const { useAzureMonitor } = require("@azure/monitor-opentelemetry");
   const { logs } = require("@opentelemetry/api-logs");
-  // const {
-  //   SemanticResourceAttributes,
-  // } = require("@opentelemetry/semantic-conventions");
 
   const startMs = parseInt(process.env.OTEL_SESSION_START || "0", 10);
   const durationMs = startMs ? Date.now() - startMs : 0;
@@ -15,11 +12,13 @@ async function post(): Promise<void> {
   console.log(`Post telemetry: duration=${durationMs}ms`);
   console.log(`Post telemetry: file=${eventsFile}`);
 
-  const workflowName = process.env.GITHUB_WORKFLOW || process.env.GITHUB_ACTION;
+  const workflowName =
+    process.env.GITHUB_WORKFLOW || process.env.GITHUB_ACTION || "";
   const runId = process.env.GITHUB_RUN_ID;
   const repo =
     process.env.GITHUB_ACTION_REPOSITORY || process.env.GITHUB_REPOSITORY;
   const actor = process.env.GITHUB_ACTOR;
+  const workflowURL = `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`;
 
   useAzureMonitor({
     azureMonitorExporterOptions: {
@@ -31,12 +30,16 @@ async function post(): Promise<void> {
   const logger = logs.getLoggerProvider().getLogger("workflow-logger", "1.0.0");
   const correlationId = process.env.OTEL_CORRELATION_ID || "";
   const tracer = trace.getTracer("workflow-tracer");
-  const span = tracer.startSpan("workflow-run", {
+  const span = tracer.startSpan(workflowName, {
     attributes: {
-      "workflow.name": workflowName,
-      "workflow.run_id": runId,
-      "workflow.repository": repo,
-      "workflow.author": actor,
+      "cicd.pipeline.action.name": workflowName,
+      "cicd.pipeline.task.run.id": runId,
+      "cicd.pipeline.task.run.url.full": workflowURL,
+      "cicd.pipeline.repository": repo,
+      "cicd.pipeline.author": actor,
+      "cicd.pipeline.duration_ms": durationMs.toString(),
+      "cicd.pipeline.result": "success", // TODO: update the value to: cancellation, error, failure, skip, success, timeout
+      "error.type": "",
     },
   });
   const operationTraceId =
@@ -65,7 +68,6 @@ async function post(): Promise<void> {
         } as Record<string, string>;
 
         if (ev.exception) {
-          // Emit as exception via logs (Azure Monitor maps exception severity)
           // logger.emit({
           //   body: ev.body || ev.name,
           //   severityNumber: 17, // Error
@@ -86,20 +88,20 @@ async function post(): Promise<void> {
         }
       }
 
-      logger.emit({
-        body: `Workflow completed in ${durationMs}ms`,
-        attributes: {
-          "microsoft.custom_event.name": "WorkflowCompleted",
-          // trace_id: operationTraceId,
-          // operation_id: operationTraceId,
-          "workflow.duration_ms": durationMs.toString(),
-          "workflow.name": workflowName,
-          "workflow.run_id": runId,
-          "workflow.repository": repo,
-          "workflow.author": actor,
-          "Service Type": "GitHub Workflow",
-        },
-      });
+      // logger.emit({
+      //   body: `Workflow completed in ${durationMs}ms`,
+      //   attributes: {
+      //     "microsoft.custom_event.name": "WorkflowCompleted",
+      //     // trace_id: operationTraceId,
+      //     // operation_id: operationTraceId,
+      //     "workflow.duration_ms": durationMs.toString(),
+      //     "workflow.name": workflowName,
+      //     "workflow.run_id": runId,
+      //     "workflow.repository": repo,
+      //     "workflow.author": actor,
+      //     "Service Type": "GitHub Workflow",
+      //   },
+      // });
     });
   } else {
     console.log("No events file found or empty");
