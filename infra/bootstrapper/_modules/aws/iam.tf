94f6a9ea2249bb8c58a3d08db849102d26bcb259
Christian Calabrese <christian.calabrese@pagopa.it>
2025-10-22 16:15:11 +0200

    Add S3 permissions to app cd role

---
+++ infra/bootstrapper/_modules/aws/iam.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "rw_docs_knowledge_base_policy" {
+  statement {
+    sid = "ListObjectsInBucket"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::dx-${var.environment.env_short}-euc1-docs-knowledge-base-s3-01",
+    ]
+  }
+
+  statement {
+    sid = "AllObjectActions"
+    effect = "Allow"
+    actions = [
+      "s3:*Object",
+    ]
+    resources = [
+      "arn:aws:s3:::dx-${var.environment.env_short}-euc1-docs-knowledge-base-s3-01/*",
+    ]
+  }
+}
+resource "aws_iam_policy" "docs_knowledge_base_policy" {
+  name        = "dx-${var.environment.env_short}-euc1-docs-knowledge-base-policy-01"
+  description = "IAM policy for MCP Server Docs Knowledge Base S3 access"
+  policy      = data.aws_iam_policy_document.rw_docs_knowledge_base_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "docs_knowledge_base_role_attachment" {
+  role       = module.bootstrap.identities.app.cd.name
+  policy_arn = aws_iam_policy.docs_knowledge_base_policy.arn
+}
