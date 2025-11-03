622d5f1d9bacb5053b28b63faab10af4b99b155d
Krusty93 <andrea.grillo@pagopa.it>
2025-11-03 12:53:30 +0100

    solve drifts

--- infra/resources/_modules/dx_website/main.tf
+++ infra/resources/_modules/dx_website/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_static_web_app" "this" {
   resource_group_name = var.resource_group_name
   location            = "westeurope"
   sku_size            = "Standard"
+  sku_tier            = "Standard"
 
   tags = var.tags
 }
