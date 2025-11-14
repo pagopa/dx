14c78670d66cf550de4c38d067840a11452de0c1
Krusty93 <andrea.grillo@pagopa.it>
2025-11-14 14:53:04 +0100

    add wallet function and appgw configuration

--- infra/resources/dev/psn.tf
+++ infra/resources/dev/psn.tf
@@ -6,8 +6,10 @@ locals {
     frontend_private_config = "private-config"
     public_listener_name    = "public-listener"
     private_listener_name   = "private-listener"
+    wallet_listener_name    = "wallet-listener"
     google_backend_pool     = "google-pool"
     bing_backend_pool       = "bing-pool"
+    wallet_backend_pool     = "wallet-pool"
     private_link_name       = "dx-d-itn-psn-appgw-pl-01"
     private_endpoint_name   = "dx-d-itn-pagopa-psn-appgw-pep-01"
   }
@@ -36,6 +38,16 @@ resource "azurerm_virtual_network" "psn_hub" {
   tags = local.tags
 }
 
+resource "azurerm_virtual_network" "psn_spoke" {
+  name                = "dx-d-itn-psn-spoke-vnet-01"
+  resource_group_name = azurerm_resource_group.psn_spoke.name
+  location            = local.location
+  address_space       = ["10.1.0.0/16"]
+
+  tags = local.tags
+}
+
+
 resource "azurerm_subnet" "psn_hub_pl" {
   name                 = "dx-d-itn-psn-pl-snet-01"
   resource_group_name  = azurerm_resource_group.psn_hub.name
@@ -50,6 +62,15 @@ resource "azurerm_subnet" "psn_hub_pep" {
   resource_group_name  = azurerm_resource_group.psn_hub.name
   virtual_network_name = azurerm_virtual_network.psn_hub.name
   address_prefixes     = ["10.254.2.0/24"]
+
+  private_link_service_network_policies_enabled = false
+}
+
+resource "azurerm_subnet" "psn_spoke_pep" {
+  name                 = "dx-d-itn-psn-pep-snet-01"
+  resource_group_name  = azurerm_resource_group.psn_spoke.name
+  virtual_network_name = azurerm_virtual_network.psn_spoke.name
+  address_prefixes     = ["10.1.2.0/24"]
 
   private_link_service_network_policies_enabled = false
 }
@@ -113,6 +134,11 @@ resource "azurerm_application_gateway" "psn" {
     fqdns = ["bing.com"]
   }
 
+  backend_address_pool {
+    name  = local.appgw.wallet_backend_pool
+    fqdns = [azurerm_linux_function_app.psn_wallet.default_hostname]
+  }
+
   backend_http_settings {
     name                  = "${local.appgw.google_backend_pool}-setting"
     cookie_based_affinity = "Disabled"
@@ -125,6 +151,15 @@ resource "azurerm_application_gateway" "psn" {
     cookie_based_affinity = "Disabled"
     port                  = 80
     protocol              = "Http"
+  }
+
+  backend_http_settings {
+    name                                = "${local.appgw.wallet_backend_pool}-setting"
+    cookie_based_affinity               = "Disabled"
+    port                                = 443
+    protocol                            = "Https"
+    probe_name                          = "${local.appgw.wallet_backend_pool}-probe"
+    pick_host_name_from_backend_address = true
   }
 
   frontend_port {
@@ -147,11 +182,13 @@ resource "azurerm_application_gateway" "psn" {
   }
 
   request_routing_rule {
-    http_listener_name          = local.appgw.public_listener_name
-    name                        = "${local.appgw.public_listener_name}-rule"
-    rule_type                   = "Basic"
-    priority                    = 10
-    redirect_configuration_name = "google"
+    http_listener_name = local.appgw.public_listener_name
+    name               = "${local.appgw.public_listener_name}-rule"
+    rule_type          = "Basic"
+    priority           = 10
+    # redirect_configuration_name = "google"
+    backend_http_settings_name = "${local.appgw.wallet_backend_pool}-setting"
+    backend_address_pool_name  = local.appgw.wallet_backend_pool
   }
 
   redirect_configuration {
@@ -176,6 +213,16 @@ resource "azurerm_application_gateway" "psn" {
     target_url           = "https://www.bing.com"
     include_path         = true
     include_query_string = true
+  }
+
+  probe {
+    name                                      = "${local.appgw.wallet_backend_pool}-probe"
+    protocol                                  = "Https"
+    path                                      = "/"
+    timeout                                   = 5
+    interval                                  = 10
+    unhealthy_threshold                       = 10
+    pick_host_name_from_backend_http_settings = true
   }
 
   private_link_configuration {
@@ -296,4 +343,298 @@ resource "azurerm_storage_blob" "asset" {
   storage_container_name = azurerm_storage_container.assets.name
   type                   = "Block"
   source                 = "${path.module}/appgw.http"
+}
+
+# PaaS
+resource "azurerm_service_plan" "psn_wallet" {
+  name                   = "dxditn-psn-wallet-asp-01"
+  location               = azurerm_resource_group.psn_spoke.location
+  resource_group_name    = azurerm_resource_group.psn_spoke.name
+  os_type                = "Linux"
+  sku_name               = "P0v4"
+  zone_balancing_enabled = false
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_account" "psn_wallet_func" {
+  name                     = "dxditnpsnwalletfnst01"
+  location                 = azurerm_resource_group.psn_spoke.location
+  resource_group_name      = azurerm_resource_group.psn_spoke.name
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+
+  public_network_access_enabled   = false
+  shared_access_key_enabled       = false
+  default_to_oauth_authentication = true
+  allow_nested_items_to_be_public = false
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.psn_hub.name
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone" "file" {
+  name                = "privatelink.file.core.windows.net"
+  resource_group_name = azurerm_resource_group.psn_hub.name
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone" "queue" {
+  name                = "privatelink.queue.core.windows.net"
+  resource_group_name = azurerm_resource_group.psn_hub.name
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone" "azurewebsites" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = azurerm_resource_group.psn_hub.name
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "blob_hub" {
+  name                  = "blob-to-hub-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.psn_hub.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "blob_spoke" {
+  name                  = "blob-to-spoke-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.psn_spoke.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "queue_hub" {
+  name                  = "queue-to-hub-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.queue.name
+  virtual_network_id    = azurerm_virtual_network.psn_hub.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "queue_spoke" {
+  name                  = "queue-to-spoke-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.queue.name
+  virtual_network_id    = azurerm_virtual_network.psn_spoke.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "file_hub" {
+  name                  = "file-to-hub-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.file.name
+  virtual_network_id    = azurerm_virtual_network.psn_hub.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "file_spoke" {
+  name                  = "file-to-spoke-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.file.name
+  virtual_network_id    = azurerm_virtual_network.psn_spoke.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "azurewebsites_hub" {
+  name                  = "azurewebsites-to-hub-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.azurewebsites.name
+  virtual_network_id    = azurerm_virtual_network.psn_hub.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "azurewebsites_spoke" {
+  name                  = "azurewebsites-to-spoke-vnet"
+  resource_group_name   = azurerm_resource_group.psn_hub.name
+  private_dns_zone_name = azurerm_private_dns_zone.azurewebsites.name
+  virtual_network_id    = azurerm_virtual_network.psn_spoke.id
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "psn_wallet_st_blob" {
+  name                = "dx-d-itn-pswn-wallet-blob-pep-01"
+  location            = azurerm_resource_group.psn_spoke.location
+  resource_group_name = azurerm_resource_group.psn_spoke.name
+  subnet_id           = azurerm_subnet.psn_spoke_pep.id
+
+  private_service_connection {
+    name                           = "dx-d-itn-pswn-wallet-blob-pep-01"
+    private_connection_resource_id = azurerm_storage_account.psn_wallet_func.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.blob.id]
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "psn_wallet_st_file" {
+  name                = "dx-d-itn-pswn-wallet-file-pep-01"
+  location            = azurerm_resource_group.psn_spoke.location
+  resource_group_name = azurerm_resource_group.psn_spoke.name
+  subnet_id           = azurerm_subnet.psn_spoke_pep.id
+
+  private_service_connection {
+    name                           = "dx-d-itn-pswn-wallet-file-pep-01"
+    private_connection_resource_id = azurerm_storage_account.psn_wallet_func.id
+    is_manual_connection           = false
+    subresource_names              = ["file"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.file.id]
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "psn_wallet_st_queue" {
+  name                = "dx-d-itn-pswn-wallet-queue-pep-01"
+  location            = azurerm_resource_group.psn_spoke.location
+  resource_group_name = azurerm_resource_group.psn_spoke.name
+  subnet_id           = azurerm_subnet.psn_spoke_pep.id
+
+  private_service_connection {
+    name                           = "dx-d-itn-pswn-wallet-queue-pep-01"
+    private_connection_resource_id = azurerm_storage_account.psn_wallet_func.id
+    is_manual_connection           = false
+    subresource_names              = ["queue"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.queue.id]
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "psn_wallet_func" {
+  name                 = "dx-d-itn-psn-wallet-func-snet-01"
+  virtual_network_name = azurerm_virtual_network.psn_spoke.name
+  resource_group_name  = azurerm_virtual_network.psn_spoke.resource_group_name
+  address_prefixes     = ["10.1.1.0/24"]
+
+  delegation {
+    name = "default"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+resource "azurerm_linux_function_app" "psn_wallet" {
+  name                = "dx-d-itn-psn-wallet-func-01"
+  location            = azurerm_resource_group.psn_spoke.location
+  resource_group_name = azurerm_resource_group.psn_spoke.name
+
+  service_plan_id = azurerm_service_plan.psn_wallet.id
+
+  storage_account_name          = azurerm_storage_account.psn_wallet_func.name
+  storage_uses_managed_identity = true
+  builtin_logging_enabled       = false
+
+  https_only                    = true
+  public_network_access_enabled = false
+  virtual_network_subnet_id     = azurerm_subnet.psn_wallet_func.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    http2_enabled                 = true
+    always_on                     = true
+    vnet_route_all_enabled        = true
+    ip_restriction_default_action = "Deny"
+    application_stack {
+      node_version = 22
+    }
+  }
+
+  app_settings = merge(
+    {
+      # https://github.com/projectkudu/kudu/wiki/Configurable-settings#attempt-to-rename-dlls-if-they-cant-be-copied-during-a-webdeploy-deployment-1
+      WEBSITE_ADD_SITENAME_BINDINGS_IN_APPHOST_CONFIG = 1
+      # https://learn.microsoft.com/en-us/azure/azure-functions/run-functions-from-deployment-package#using-website_run_from_package--1
+      WEBSITE_RUN_FROM_PACKAGE = 1
+      # https://docs.microsoft.com/en-us/azure/virtual-network/what-is-ip-address-168-63-129-16
+      WEBSITE_DNS_SERVER = "168.63.129.16"
+    },
+  )
+
+  tags = local.tags
+
+  depends_on = [
+    azurerm_private_endpoint.psn_wallet_st_blob,
+    azurerm_private_endpoint.psn_wallet_st_file,
+    azurerm_private_endpoint.psn_wallet_st_queue,
+  ]
+}
+
+resource "azurerm_private_endpoint" "psn_wallet_func" {
+  name                = "dx-d-itn-psn-wallet-func-pep-01"
+  location            = azurerm_resource_group.psn_hub.location
+  resource_group_name = azurerm_resource_group.psn_hub.name
+  subnet_id           = azurerm_subnet.psn_hub_pep.id
+
+  private_service_connection {
+    name                           = "dx-d-itn-psn-wallet-func-pep-01"
+    private_connection_resource_id = azurerm_linux_function_app.psn_wallet.id
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.azurewebsites.id]
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_role_assignment" "function_storage_blob_data_owner" {
+  scope                = azurerm_storage_account.psn_wallet_func.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_linux_function_app.psn_wallet.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "function_storage_account_contributor" {
+  scope                = azurerm_storage_account.psn_wallet_func.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_linux_function_app.psn_wallet.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "function_storage_queue_data_contributor" {
+  scope                = azurerm_storage_account.psn_wallet_func.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_linux_function_app.psn_wallet.identity[0].principal_id
 }
