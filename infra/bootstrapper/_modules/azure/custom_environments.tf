b0b4511435b0b131eed4ed1a328799fa96df4ae9
Krusty93 <andrea.grillo@pagopa.it>
2025-11-17 12:29:14 +0100

    update env

--- infra/bootstrapper/_modules/azure/custom_environments.tf
+++ infra/bootstrapper/_modules/azure/custom_environments.tf
@@ -2,12 +2,12 @@ resource "github_actions_environment_secret" "integration_tests_client_id" {
   repository      = var.repository.name
   environment     = "integration-tests"
   secret_name     = "ARM_CLIENT_ID"
-  encrypted_value = base64sha256(module.bootstrap.identities.infra.cd.client_id)
+  plaintext_value = module.bootstrap.identities.infra.cd.client_id
 }
 
 resource "github_actions_environment_secret" "integration_tests_subscription_id" {
   repository      = var.repository.name
   environment     = "integration-tests"
   secret_name     = "ARM_SUBSCRIPTION_ID"
-  encrypted_value = base64sha256(module.bootstrap.subscription_id)
+  plaintext_value = module.bootstrap.subscription_id
 }
