34ce46125525a174ff848782fab79f4a645c1da0
Marco Comi <marco.comi@pagopa.it>
2025-12-04 10:44:34 +0100

    Improve the decode function to better have type support by IDE
    The previous version worked, but the IDE did not show the proper type

--- apps/cli/src/adapters/zod/index.ts
+++ apps/cli/src/adapters/zod/index.ts
@@ -1,9 +1,11 @@
 import { ResultAsync } from "neverthrow";
 import { z } from "zod/v4";
 
-export const decode = <T>(schema: z.ZodSchema<T>) =>
-  ResultAsync.fromThrowable(
-    schema.parseAsync,
-    (cause) =>
-      new Error("File content is not valid for the given schema", { cause }),
-  );
+export const decode =
+  <T>(schema: z.ZodSchema<T>) =>
+  (data: unknown): ResultAsync<T, Error> =>
+    ResultAsync.fromPromise(
+      schema.parseAsync(data),
+      (cause) =>
+        new Error("Input is not valid for the given schema", { cause }),
+    );
