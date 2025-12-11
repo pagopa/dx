22d988c550612a17dfdb7f7490b19d5ca5b8b6f1
Christian Calabrese <christian.calabrese@pagopa.it>
2025-12-11 09:23:54 +0000

    Add UAT env to repository configuration

--- infra/repository/main.tf
+++ infra/repository/main.tf
@@ -16,7 +16,7 @@ module "github_repository" {
     app_cd_policy_tags     = ["@pagopa/dx-mcpserver@**"]
     homepage_url           = "https://dx.pagopa.it"
     pull_request_bypassers = ["/dx-pagopa-bot"]
-    environments           = ["dev", "prod"]
+    environments           = ["dev", "uat", "prod"]
     jira_boards_ids        = ["CES"]
   }
 }
