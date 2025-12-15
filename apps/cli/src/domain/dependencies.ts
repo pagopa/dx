29d31bef316325b394f29714aa63fdecb07f037b
Marco Comi <marco.comi@pagopa.it>
2025-12-15 11:43:02 +0100

    Use gh CLI to create Pull Request
    # Conflicts:
    #	apps/cli/src/adapters/commander/commands/init.ts

--- apps/cli/src/domain/dependencies.ts
+++ apps/cli/src/domain/dependencies.ts
@@ -1,11 +1,8 @@
-import { Octokit } from "octokit";
-
 import { PackageJsonReader } from "./package-json.js";
 import { RepositoryReader } from "./repository.js";
 import { ValidationReporter } from "./validation.js";
 
 export type Dependencies = {
-  octokit: Octokit;
   packageJsonReader: PackageJsonReader;
   repositoryReader: RepositoryReader;
   validationReporter: ValidationReporter;
