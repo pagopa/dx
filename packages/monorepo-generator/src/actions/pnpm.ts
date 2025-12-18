c7a37e61a68d0893204a85079dd53a7094b1a024
Marco Comi <marco.comi@pagopa.it>
2025-12-18 11:23:48 +0100

    Fix issue with pnpm configuration actions

--- packages/monorepo-generator/src/actions/pnpm.ts
+++ packages/monorepo-generator/src/actions/pnpm.ts
@@ -1,38 +1,19 @@
 import type { ActionType } from "plop";
 
 import { $ } from "execa";
+import path from "node:path";
 
-export const enablePnpm: ActionType = async (answers) =>
-  $({
-    cwd: `${answers.repoSrc}/${answers.repoName}`,
-  })`corepack use pnpm@latest`
-    .then(() => "pnpm enabled")
-    .catch(() => "Error enabling pnpm");
+import { Answers } from "../index.js";
 
-export const addPagoPaPnpmPlugin: ActionType = async (answers) =>
-  $({
-    cwd: `${answers.repoSrc}/${answers.repoName}`,
-  })`pnpm add --config pnpm-plugin-pagopa`
-    .then(() => "pnpm-plugin-pagopa added")
-    .catch(() => "Error enabling pnpm-plugin-pagopa");
-
-export const installRootDependencies: ActionType = async (answers) =>
-  $({
-    cwd: `${answers.repoSrc}/${answers.repoName}`,
-  })`pnpm -w add -D turbo @changesets/cli @devcontainers/cli`
-    .then(() => "Root dependencies installed")
-    .catch(() => "Error installing root dependencies");
-
-export const configureChangesets: ActionType = async (answers) =>
-  $({
-    cwd: `${answers.repoSrc}/${answers.repoName}`,
-  })`pnpm changeset init`
-    .then(() => "Changeset configured")
-    .catch(() => "Error configuring changeset");
-
-export const configureDevContainer: ActionType = async (answers) =>
-  $({
-    cwd: `${answers.repoSrc}/${answers.repoName}`,
-  })`pnpm devcontainer templates apply -t ghcr.io/pagopa/devcontainer-templates/node:1`
-    .then(() => "DevContainer configured")
-    .catch(() => "Error configuring DevContainer");
+export const setupMonorepoWithPnpm =
+  ({ repoName }: Pick<Answers, "repoName">): ActionType =>
+  async () => {
+    const cwd = path.resolve(repoName);
+    const pnpm$ = $({ cwd });
+    await pnpm$`corepack use pnpm@latest`;
+    await pnpm$`pnpm add --config pnpm-plugin-pagopa`;
+    await pnpm$`pnpm -w add -D turbo @changesets/cli @devcontainers/cli`;
+    await pnpm$`pnpm changeset init`;
+    await pnpm$`pnpm devcontainer templates apply -t ghcr.io/pagopa/devcontainer-templates/node:1`;
+    return "Monorepo bootstrapped";
+  };
