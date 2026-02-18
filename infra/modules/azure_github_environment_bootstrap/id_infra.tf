0b58439ec064ab8aff1e4ba36845e671ddf4d22c
Krusty93 <andrea.grillo@pagopa.it>
2026-02-18 15:18:29 +0100

    remove deprecated property

--- infra/modules/azure_github_environment_bootstrap/id_infra.tf
+++ infra/modules/azure_github_environment_bootstrap/id_infra.tf
@@ -15,19 +15,17 @@ resource "azurerm_user_assigned_identity" "infra_cd" {
 }
 
 resource "azurerm_federated_identity_credential" "github_infra_ci" {
-  resource_group_name = azurerm_user_assigned_identity.infra_ci.resource_group_name
-  name                = format(local.ids.federated_identity_name, "infra", "ci")
-  audience            = local.ids.audience
-  issuer              = local.ids.issuer
-  parent_id           = azurerm_user_assigned_identity.infra_ci.id
-  subject             = "repo:pagopa/${var.repository.name}:environment:${format(local.ids.infra_environment_name, "ci")}"
+  name      = format(local.ids.federated_identity_name, "infra", "ci")
+  audience  = local.ids.audience
+  issuer    = local.ids.issuer
+  parent_id = azurerm_user_assigned_identity.infra_ci.id
+  subject   = "repo:pagopa/${var.repository.name}:environment:${format(local.ids.infra_environment_name, "ci")}"
 }
 
 resource "azurerm_federated_identity_credential" "github_infra_cd" {
-  resource_group_name = azurerm_user_assigned_identity.infra_cd.resource_group_name
-  name                = format(local.ids.federated_identity_name, "infra", "cd")
-  audience            = local.ids.audience
-  issuer              = local.ids.issuer
-  parent_id           = azurerm_user_assigned_identity.infra_cd.id
-  subject             = "repo:pagopa/${var.repository.name}:environment:${format(local.ids.infra_environment_name, "cd")}"
+  name      = format(local.ids.federated_identity_name, "infra", "cd")
+  audience  = local.ids.audience
+  issuer    = local.ids.issuer
+  parent_id = azurerm_user_assigned_identity.infra_cd.id
+  subject   = "repo:pagopa/${var.repository.name}:environment:${format(local.ids.infra_environment_name, "cd")}"
 }
