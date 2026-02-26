ac21acd139b25d4c275398fe087362ab1651bedc
Krusty93 <andrea.grillo@pagopa.it>
2026-02-26 17:56:20 +0100

    add envs

--- infra/repository/environments.tf
+++ infra/repository/environments.tf
@@ -1,5 +1,5 @@
 resource "github_repository_environment" "automated_tests" {
-  environment = "main"
+  environment = "integration-tests"
   repository  = module.github_repository.name
 
   deployment_branch_policy {
@@ -10,6 +10,36 @@ resource "github_repository_environment" "automated_tests" {
 
 resource "github_repository_environment" "npm_prod_cd" {
   environment = "npm-prod-cd"
+  repository  = module.github_repository.name
+
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
+
+resource "github_repository_environment" "certificates_dev_cd" {
+  environment = "certificates-dev-cd"
+  repository  = module.github_repository.name
+
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
+
+resource "github_repository_environment" "certificates_uat_cd" {
+  environment = "certificates-uat-cd"
+  repository  = module.github_repository.name
+
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
+
+resource "github_repository_environment" "certificates_prod_cd" {
+  environment = "certificates-prod-cd"
   repository  = module.github_repository.name
 
   deployment_branch_policy {
