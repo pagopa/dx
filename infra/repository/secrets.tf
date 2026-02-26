0de723502a7622832e29db8ae50210959428b1a3
Krusty93 <andrea.grillo@pagopa.it>
2026-02-26 17:20:58 +0100

    add secrets to repository

--- infra/repository/secrets.tf
+++ infra/repository/secrets.tf
@@ -7,3 +7,23 @@ resource "github_actions_secret" "slack_webhook_url" {
     ignore_changes = [remote_updated_at]
   }
 }
+
+resource "github_actions_secret" "le_private_key_json" {
+  repository      = module.github_repository.name
+  secret_name     = "LE_PRIVATE_KEY_JSON"
+  plaintext_value = "placeholder"
+
+  lifecycle {
+    ignore_changes = [remote_updated_at]
+  }
+}
+
+resource "github_actions_secret" "le_regr_json" {
+  repository      = module.github_repository.name
+  secret_name     = "LE_REGR_JSON"
+  plaintext_value = "placeholder"
+
+  lifecycle {
+    ignore_changes = [remote_updated_at]
+  }
+}
