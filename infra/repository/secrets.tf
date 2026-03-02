fcd8b415bfb617536cf26fff054f42975580fb1f
Andrea Grillo <andrea.grillo@pagopa.it>
2026-03-02 17:54:49 +0100

    Add automation GitHub environments and Let's Encrypt secrets (#1368)

--- infra/repository/secrets.tf
+++ infra/repository/secrets.tf
@@ -7,3 +7,23 @@ resource "github_actions_secret" "slack_webhook_url" {
     ignore_changes = [remote_updated_at]
   }
 }
+
+resource "github_actions_secret" "lets_encrypt_private_key" {
+  repository      = module.github_repository.name
+  secret_name     = "LETS_ENCRYPT_PRIVATE_KEY_JSON"
+  plaintext_value = "placeholder"
+
+  lifecycle {
+    ignore_changes = [remote_updated_at]
+  }
+}
+
+resource "github_actions_secret" "lets_encrypt_registration" {
+  repository      = module.github_repository.name
+  secret_name     = "LETS_ENCRYPT_REGISTRATION_JSON"
+  plaintext_value = "placeholder"
+
+  lifecycle {
+    ignore_changes = [remote_updated_at]
+  }
+}
