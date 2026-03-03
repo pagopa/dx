dc100c81645f196dbd98aaf11192777026ecc438
Krusty93 <andrea.grillo@pagopa.it>
2026-03-03 12:33:07 +0100

    add google oidc config

--- infra/core/_modules/stategraph/locals.tf
+++ infra/core/_modules/stategraph/locals.tf
@@ -1,14 +1,19 @@
 locals {
   environment_variables = {
-    "DB_HOST"            = azurerm_postgresql_flexible_server.stategraph.fqdn,
-    "DB_PORT"            = "5432"
-    "DB_USER"            = "stategraph",
-    "DB_NAME"            = "stategraph",
-    "STATEGRAPH_UI_BASE" = "https://stategraph.${var.dns.zone_name}",
-    "POSTGRES_DB"        = "stategraph",
+    "DB_HOST"                        = azurerm_postgresql_flexible_server.stategraph.fqdn,
+    "DB_PORT"                        = "5432"
+    "DB_USER"                        = "stategraph",
+    "DB_NAME"                        = "stategraph",
+    "STATEGRAPH_UI_BASE"             = "https://stategraph.${var.dns.zone_name}",
+    "STATEGRAPH_OAUTH_REDIRECT_BASE" = "https://stategraph.${var.dns.zone_name}",
+    "POSTGRES_DB"                    = "stategraph",
+    "STATEGRAPH_OAUTH_TYPE"          = "google",
+    "STATEGRAPH_OAUTH_EMAIL_DOMAIN"  = "pagopa.it"
   }
 
   environment_secrets = [
     "DB_PASS",
+    "STATEGRAPH_OAUTH_CLIENT_ID",
+    "STATEGRAPH_OAUTH_CLIENT_SECRET"
   ]
 }
