5ffa2cede11a4142bf666682eb22cfebe876e48a
Krusty93 <andrea.grillo@pagopa.it>
2026-03-04 11:46:53 +0100

    restore integration identity

--- infra/bootstrapper/_modules/azure/custom_environments.tf
+++ infra/bootstrapper/_modules/azure/custom_environments.tf
@@ -2,8 +2,8 @@ resource "github_actions_environment_secret" "integration_tests_client_id" {
   count = var.environment.env_short == "d" ? 1 : 0
 
   repository      = var.repository.name
-  environment     = "integration-tests"
-  secret_name     = "ARM_CLIENT_ID"
+  environment     = "automation-dev-cd"
+  secret_name     = "INTEGRATION_ARM_CLIENT_ID"
   plaintext_value = azurerm_user_assigned_identity.integration_tests[0].client_id
 }
 
@@ -11,21 +11,7 @@ resource "github_actions_environment_secret" "integration_tests_subscription_id"
   count = var.environment.env_short == "d" ? 1 : 0
 
   repository      = var.repository.name
-  environment     = "integration-tests"
-  secret_name     = "ARM_SUBSCRIPTION_ID"
-  plaintext_value = module.bootstrap.subscription_id
-}
-
-resource "github_actions_environment_secret" "automation_cd_client_id" {
-  repository      = var.repository.name
-  environment     = "automation-${local.env_long}-cd"
-  secret_name     = "ARM_CLIENT_ID"
-  plaintext_value = module.bootstrap.identities.infra.cd.client_id
-}
-
-resource "github_actions_environment_secret" "automation_cd_subscription_id" {
-  repository      = var.repository.name
-  environment     = "automation-${local.env_long}-cd"
-  secret_name     = "ARM_SUBSCRIPTION_ID"
+  environment     = "automation-dev-cd"
+  secret_name     = "INTEGRATION_ARM_SUBSCRIPTION_ID"
   plaintext_value = module.bootstrap.subscription_id
 }
