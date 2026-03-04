a1f39b3142b267fb8dec2dcb8f8150a02d67227c
Andrea Grillo <andrea.grillo@pagopa.it>
2026-03-04 18:09:39 +0100

    Fix Terraform integration test setup (#1393)

--- infra/repository/environments.tf
+++ infra/repository/environments.tf
@@ -1,13 +1,3 @@
-resource "github_repository_environment" "automated_tests" {
-  environment = "integration-tests"
-  repository  = module.github_repository.name
-
-  deployment_branch_policy {
-    protected_branches     = true
-    custom_branch_policies = false
-  }
-}
-
 resource "github_repository_environment" "npm_prod_cd" {
   environment = "npm-prod-cd"
   repository  = module.github_repository.name
