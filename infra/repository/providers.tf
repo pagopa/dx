8adccb9e6c53174b7e6224e41dd218699d845adb
Krusty93 <andrea.grillo@pagopa.it>
2026-03-05 10:38:47 +0100

    switch backend

--- infra/repository/providers.tf
+++ infra/repository/providers.tf
@@ -6,11 +6,8 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "dx-d-itn-tfstate-rg-01"
-    storage_account_name = "dxditntfstatest01"
-    container_name       = "terraform-state"
-    key                  = "dx.repository.tfstate"
+  backend "http" {
+    address = "https://stategraph.dev.dx.pagopa.it/api/v1/states/backend/4a7c9de0-24e2-4d76-8dc9-5c3775a9d87f"
   }
 }
 
