08ae0b37b7dc08162cb8618da3f78fadd1773e83
Danilo Spinelli <danilo.spinelli@pagopa.it>
2026-03-11 07:10:47 +0000

    Refactor code structure for improved readability and maintainability

--- apps/dx-metrics/eslint.config.mjs
+++ apps/dx-metrics/eslint.config.mjs
@@ -2,17 +2,4 @@ import lintRules from "@pagopa/eslint-config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import { defineConfig } from "eslint/config";
 
-// pnpm resolves two separate instances of @typescript-eslint/eslint-plugin
-// (one for eslint-config-next, one for @pagopa/eslint-config), causing ESLint
-// to throw "Cannot redefine plugin". We normalise all configs to share the
-// single plugin instance that nextVitals already registers.
-const tsPlugin = nextVitals.find((c) => c.plugins?.["@typescript-eslint"])
-  ?.plugins?.["@typescript-eslint"];
-
-const normalizedLintRules = lintRules.map((rule) =>
-  rule.plugins?.["@typescript-eslint"]
-    ? { ...rule, plugins: { ...rule.plugins, "@typescript-eslint": tsPlugin } }
-    : rule,
-);
-
-export default defineConfig(...nextVitals, ...normalizedLintRules);
+export default defineConfig(...nextVitals, ...lintRules);
