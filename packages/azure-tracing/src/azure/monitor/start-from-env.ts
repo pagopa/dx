a5f3cb1b3747f76d90af8774e1e2cf5eb4f3dcce
Marco Comi <marco.comi@pagopa.it>
2026-03-24 15:51:08 +0100

    Upgrade OpenTelemetry dependencies

--- packages/azure-tracing/src/azure/monitor/start-from-env.ts
+++ packages/azure-tracing/src/azure/monitor/start-from-env.ts
@@ -11,5 +11,8 @@ export const initFromEnv = () => {
     },
     enableLiveMetrics: true,
     samplingRatio: env.APPINSIGHTS_SAMPLING_PERCENTAGE,
+    // Disable the RateLimitedSampler (new default in 1.16.0) so that
+    // samplingRatio is respected via the ApplicationInsightsSampler.
+    tracesPerSecond: 0,
   });
 };
