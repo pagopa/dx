317e2245b2098d2eee8df9524f3779326240d591
Luca Cavallaro <luca.cavallaro@pagopa.it>
2026-04-14 15:11:43 +0000

    Exclude tests and examples modules from Nx graph

--- packages/nx-terraform-plugin/src/index.ts
+++ packages/nx-terraform-plugin/src/index.ts
@@ -2,11 +2,14 @@ import {
   CreateDependencies,
   createNodesFromFiles,
   CreateNodesV2,
+  ProjectType,
 } from "@nx/devkit";
 import path from "node:path";
 
 import { getStaticDependenciesFromFile } from "./fs.ts";
 import { getProjectNameFromRoot, getTerraformProjectFiles } from "./project.ts";
+
+const ignoreModules = ["tests", "_tests", "examples", "example"];
 
 export const createNodesV2: CreateNodesV2<unknown> = [
   // We create a terraform project for each directory containing .tf files
@@ -15,6 +18,15 @@ export const createNodesV2: CreateNodesV2<unknown> = [
     await createNodesFromFiles(
       (configFile) => {
         const root = path.dirname(configFile);
+
+        const rootSegments = new Set(root.split(path.sep));
+
+        if (ignoreModules.some((module) => rootSegments.has(module))) {
+          return {
+            projects: {},
+          };
+        }
+
         const name = getProjectNameFromRoot(root);
         return {
           projects: {
