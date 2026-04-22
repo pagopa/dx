9c1c84124e12bb5a1cb9ab50d123f5fb3a66d54d
Marco Comi <marco.comi@pagopa.it>
2026-04-22 17:18:57 +0200

    Add endpoint as output

--- infra/modules/azure_managed_redis/outputs.tf
+++ infra/modules/azure_managed_redis/outputs.tf
@@ -32,3 +32,8 @@ output "private_endpoint_ip_address" {
   description = "The private IP address assigned to the Managed Redis private endpoint, or null when public networking is in use."
   value       = try(azurerm_private_endpoint.redis[0].private_dns_zone_configs[0].record_sets[0].ip_addresses[0], null)
 }
+
+output "endpoint" {
+  description = "The full endpoint (hostname:port) of the Azure Managed Redis instance."
+  value       = "${azurerm_managed_redis.this.hostname}:${azurerm_managed_redis.this.default_database[0].port}"
+}
