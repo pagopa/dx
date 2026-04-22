7c5de975c90981baa4f2458dd7d9f96390dab9fc
Christian Calabrese <christian.calabrese@pagopa.it>
2026-04-22 15:54:10 +0200

    Added zones input to public ip

--- infra/modules/azure_core_infra/_modules/vpn/vpn.tf
+++ infra/modules/azure_core_infra/_modules/vpn/vpn.tf
@@ -8,6 +8,7 @@ resource "azurerm_public_ip" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  zones               = ["1", "2"]
   tags                = var.tags
 }
 
