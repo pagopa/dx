f2f0c7702827dc7256a52165934afd4e28994237
Christian Calabrese <christian.calabrese@pagopa.it>
2026-04-24 17:24:19 +0200

    Substitute single role assignments with merged roles in azure bootstrapper module

--- infra/modules/azure_github_environment_bootstrap/id_app_cd_iam.tf
+++ infra/modules/azure_github_environment_bootstrap/id_app_cd_iam.tf
@@ -7,40 +7,13 @@ resource "azurerm_role_assignment" "app_cd_subscription_reader" {
 }
 
 # Resource Group
-resource "azurerm_role_assignment" "app_cd_rgs_website_contributor" {
+resource "azurerm_role_assignment" "app_cd_rgs_deploy" {
   for_each = local.resource_group_ids
 
   scope                = each.value
-  role_definition_name = "Website Contributor"
+  role_definition_name = "PagoPA DX App CD Resource Group Deploy"
   principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
-  description          = "Allow ${var.repository.name} App CD identity to deploy code to AppService and Function Apps at ${each.value} resource group scope"
-}
-
-resource "azurerm_role_assignment" "app_cd_rgs_cdn_profile_contributor" {
-  for_each = local.resource_group_ids
-
-  scope                = each.value
-  role_definition_name = "CDN Profile Contributor"
-  principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
-  description          = "Allow ${var.repository.name} App CD identity to apply purge CDNs at ${each.value} resource group scope"
-}
-
-resource "azurerm_role_assignment" "app_cd_rgs_cae_contributor" {
-  for_each = local.resource_group_ids
-
-  scope                = each.value
-  role_definition_name = "Container Apps Contributor"
-  principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
-  description          = "Allow ${var.repository.name} App CD identity to apply changes to Container App Environment at ${each.value} resource group scope"
-}
-
-resource "azurerm_role_assignment" "app_cd_rgs_blob_contributor" {
-  for_each = local.resource_group_ids
-
-  scope                = each.value
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
-  description          = "Allow ${var.repository.name} App CD identity to apply changes to resources at ${each.value} resource group scope"
+  description          = "Allow ${var.repository.name} App CD identity to apply the DX deploy role at ${each.value} resource group scope"
 }
 
 resource "azurerm_role_assignment" "app_cd_tf_rg_blob_contributor" {
@@ -48,13 +21,4 @@ resource "azurerm_role_assignment" "app_cd_tf_rg_blob_contributor" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
   description          = "Allow ${var.repository.name} App CD identity to apply changes to the Terraform state file Storage Account scope"
-}
-
-resource "azurerm_role_assignment" "app_cd_rgs_static_webapp_secrets" {
-  for_each = local.resource_group_ids
-
-  scope                = each.value
-  role_definition_name = "PagoPA Static Web Apps List Secrets"
-  principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
-  description          = "Allow ${var.repository.name} App CD identity to to read Static Web Apps secrets at ${each.value} resource group scope"
 }
