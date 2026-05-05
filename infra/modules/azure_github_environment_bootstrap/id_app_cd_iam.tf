be41ba00e9dd56916463396532c8204b6b018792
Christian Calabrese <christian.calabrese@pagopa.it>
2026-05-05 10:21:45 +0200

    Make the update a major

--- infra/modules/azure_github_environment_bootstrap/id_app_cd_iam.tf
+++ infra/modules/azure_github_environment_bootstrap/id_app_cd_iam.tf
@@ -11,7 +11,7 @@ resource "azurerm_role_assignment" "app_cd_rgs_deploy" {
   for_each = local.resource_group_ids
 
   scope                = each.value
-  role_definition_name = "PagoPA DX App CD Resource Group Deploy"
+  role_definition_name = "DX App CD Resource Groups"
   principal_id         = azurerm_user_assigned_identity.app_cd.principal_id
   description          = "Allow ${var.repository.name} App CD identity to apply the DX deploy role at ${each.value} resource group scope"
 }
--- infra/modules/azure_github_environment_bootstrap/id_infra_ci_iam.tf
+++ infra/modules/azure_github_environment_bootstrap/id_infra_ci_iam.tf
@@ -1,7 +1,7 @@
 # Subscription
 resource "azurerm_role_assignment" "infra_ci_subscription_reader" {
   scope                = var.subscription_id
-  role_definition_name = "PagoPA DX Infra CI Subscription Reader"
+  role_definition_name = "DX Infra CI Subscription"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
   description          = "Allow ${var.repository.name} Infra CI identity to read DX repository resources at subscription scope"
 }
@@ -19,7 +19,7 @@ resource "azurerm_role_assignment" "infra_ci_rgs_reader" {
   for_each = local.resource_group_ids
 
   scope                = each.value
-  role_definition_name = "PagoPA DX Infra CI Resource Group Reader"
+  role_definition_name = "DX Infra CI Resource Groups"
   principal_id         = azurerm_user_assigned_identity.infra_ci.principal_id
   description          = "Allow ${var.repository.name} Infra CI identity to read the DX resource group bundle at ${each.value} scope"
 }
