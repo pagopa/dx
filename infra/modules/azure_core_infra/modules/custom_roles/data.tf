30fdf8f0f85b54b2e1f63f8bfc3464dde362345a
Christian Calabrese <christian.calabrese@pagopa.it>
2026-05-11 11:20:50 +0200

    Fix usage of subscription ids

--- infra/modules/azure_core_infra/modules/custom_roles/data.tf
+++ infra/modules/azure_core_infra/modules/custom_roles/data.tf
@@ -1,3 +1,3 @@
 data "azurerm_subscription" "current" {
-  count = var.subscription_id == null ? 0 : 1
+  count = var.subscription_id == null ? 1 : 0
 }
--- infra/modules/azure_core_infra/modules/custom_roles/locals.tf
+++ infra/modules/azure_core_infra/modules/custom_roles/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  subscription_id   = var.subscription_id != null ? var.subscription_id : data.azurerm_subscription.current[0].subscription_id
+  subscription_id   = var.subscription_id != null ? var.subscription_id : data.azurerm_subscription.current[0].id
   subscription_name = var.subscription_name != null ? var.subscription_name : data.azurerm_subscription.current[0].display_name
 
   subscription_role_name_prefix = trimspace(local.subscription_name)
