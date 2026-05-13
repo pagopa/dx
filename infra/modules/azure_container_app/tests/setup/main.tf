eb1063f6268b59c370c5154714dfd9d67b8f70fb
Krusty93 <andrea.grillo@pagopa.it>
2026-05-13 16:49:31 +0200

    add tests

--- infra/modules/azure_container_app/tests/setup/main.tf
+++ infra/modules/azure_container_app/tests/setup/main.tf
@@ -1,128 +1,96 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.110, < 5.0"
-    }
-    dx = {
-      source  = "pagopa-dx/azure"
-      version = ">= 0.0.6, < 1.0.0"
-    }
-  }
-}
-
 locals {
-  environment = {
-    prefix          = "dx"
-    env_short       = "d"
-    location        = "italynorth"
-    domain          = "modules"
-    app_name        = "test"
-    instance_number = "01"
-  }
-  tags = {
-    CostCenter     = "TS000 - Tecnologia e Servizi"
-    CreatedBy      = "Terraform"
-    Environment    = "Dev"
-    BusinessUnit   = "DevEx"
-    Source         = "https://github.com/pagopa/dx/blob/main/infra/modules/azure_container_app/tests"
-    ManagementTeam = "Developer Experience"
-    Test           = "true"
-    TestName       = "Create Container App for test"
-  }
-
   naming_config = {
-    prefix          = var.environment.prefix,
-    environment     = var.environment.env_short,
+    prefix          = var.environment.prefix
+    environment     = var.environment.env_short
     location        = var.environment.location
-    name            = var.environment.app_name,
-    instance_number = tonumber(var.environment.instance_number),
+    domain          = var.environment.domain
+    name            = var.environment.app_name
+    instance_number = tonumber(var.environment.instance_number)
+  }
+
+  existing_resources = {
+    prefix          = var.environment.prefix
+    environment     = var.environment.env_short
+    location        = var.environment.location
+    domain          = ""
+    name            = var.test_kind
+    instance_number = tonumber(var.environment.instance_number)
   }
 }
 
-data "azurerm_resource_group" "rg" {
-  name = provider::dx::resource_name(merge(local.naming_config, {
-    name          = "test",
-    resource_type = "resource_group"
-  }))
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "test" {
+  name = provider::dx::resource_name(merge(local.existing_resources, { resource_type = "resource_group" }))
 }
 
-data "azurerm_container_app_environment" "cae" {
-  name = provider::dx::resource_name(merge(local.naming_config, {
-    name          = "github-runner",
-    resource_type = "container_app_environment"
-  }))
-  resource_group_name = provider::dx::resource_name(merge(local.naming_config, {
-    name          = "github-runner",
-    resource_type = "resource_group"
-  }))
+data "azurerm_log_analytics_workspace" "logs" {
+  name                = provider::dx::resource_name(merge(local.existing_resources, { resource_type = "log_analytics" }))
+  resource_group_name = data.azurerm_resource_group.test.name
 }
 
-data "azurerm_key_vault" "kv" {
-  name = provider::dx::resource_name(merge(local.naming_config, {
-    name          = "common",
-    resource_type = "key_vault"
-  }))
-  resource_group_name = provider::dx::resource_name(merge(local.naming_config, {
-    name          = "common",
-    resource_type = "resource_group"
-  }))
+resource "azurerm_resource_group" "sut" {
+  name     = provider::dx::resource_name(merge(local.naming_config, { resource_type = "resource_group" }))
+  location = var.environment.location
+
+  tags = var.tags
 }
 
-resource "azurerm_key_vault_secret" "test1" {
-  name            = "TESTSECRET1"
-  value           = "value1"
-  key_vault_id    = data.azurerm_key_vault.kv.id
-  expiration_date = timeadd(timestamp(), "30m")
-  content_type    = "application/text"
+# Random base instance number regenerated on every test run to ensure isolation
+# across concurrent or repeated test executions.
+# Base range (10–24) ensures all derived instance numbers stay within 10–99.
+resource "random_integer" "instance_base" {
+  min = 10
+  max = 24
+  keepers = {
+    run_timestamp = timestamp()
+  }
 }
 
-resource "azurerm_key_vault_secret" "test2" {
-  name            = "TEST-SECRET2"
-  value           = "value2"
-  key_vault_id    = data.azurerm_key_vault.kv.id
-  expiration_date = timeadd(timestamp(), "30m")
-  content_type    = "application/text"
+resource "azurerm_container_app_environment" "sut" {
+  name                       = provider::dx::resource_name(merge(local.naming_config, { resource_type = "container_app_environment" }))
+  resource_group_name        = azurerm_resource_group.sut.name
+  location                   = var.environment.location
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.logs.id
+
+  workload_profile {
+    name                  = "Consumption"
+    workload_profile_type = "Consumption"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      infrastructure_resource_group_name,
+    ]
+  }
+
+  timeouts {
+    create = "60m"
+  }
+
+  tags = var.tags
 }
 
-resource "azurerm_user_assigned_identity" "cae" {
-  name                = provider::dx::resource_name(merge(local.naming_config, { resource_type = "managed_identity" }))
-  location            = local.environment.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  tags = local.tags
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
 }
 
 output "resource_group_name" {
-  value = data.azurerm_resource_group.rg.name
+  value = azurerm_resource_group.sut.name
 }
 
 output "container_app_environment_id" {
-  value = data.azurerm_container_app_environment.cae.id
+  value = azurerm_container_app_environment.sut.id
 }
 
-output "key_vault_secret1" {
+output "log_analytics_workspace_id" {
+  value = data.azurerm_log_analytics_workspace.logs.id
+}
+
+output "instance_numbers" {
   value = {
-    secret_id = azurerm_key_vault_secret.test1.versionless_id
-    name      = "${azurerm_key_vault_secret.test1.name}_SECRET"
+    default     = tostring(random_integer.instance_base.result)
+    development = tostring(random_integer.instance_base.result + 25)
+    autoscaler  = tostring(random_integer.instance_base.result + 50)
   }
-}
-
-output "key_vault_secret2" {
-  value = {
-    secret_id = azurerm_key_vault_secret.test2.versionless_id
-    name      = azurerm_key_vault_secret.test2.name
-  }
-}
-
-output "tags" {
-  value = local.tags
-}
-
-output "environment" {
-  value = local.environment
-}
-
-output "user_assigned_identity_id" {
-  value = azurerm_user_assigned_identity.cae.id
 }
