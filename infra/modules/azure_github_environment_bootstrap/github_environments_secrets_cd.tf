ef793a0be5b106eb0977c6c57fc6a16cce96e58f
Andrea Grillo <andrea.grillo@pagopa.it>
2026-06-23 16:29:45 +0200

    Add automation-cd environment to azure_github_environment_bootstrap (#1872)

--- infra/modules/azure_github_environment_bootstrap/github_environments_secrets_cd.tf
+++ infra/modules/azure_github_environment_bootstrap/github_environments_secrets_cd.tf
@@ -9,6 +9,16 @@ resource "github_actions_environment_secret" "infra_cd" {
 }
 
 #trivy:ignore:AVD-GIT-0002
+resource "github_actions_environment_secret" "automation_cd" {
+  for_each = local.automation_cd.secrets
+
+  repository      = var.repository.name
+  environment     = "automation-${local.env_name}-cd"
+  secret_name     = each.key
+  plaintext_value = each.value
+}
+
+#trivy:ignore:AVD-GIT-0002
 resource "github_actions_environment_secret" "app_cd" {
   for_each = local.app_cd.secrets
 
