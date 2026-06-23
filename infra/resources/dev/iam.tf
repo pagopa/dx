a0165f0756d5e1acb1c482f727a1f782ab861836
Mario Mupo <mario.mupo@pagopa.it>
2026-06-23 12:28:08 +0200

    fix: remove non necessary input, use right identity

--- infra/resources/dev/iam.tf
+++ infra/resources/dev/iam.tf
@@ -5,18 +5,18 @@ resource "azurerm_role_assignment" "infra_cd_key_vault_crypto" {
   description          = "Allow Infra CD Identity to manage KeyVault keys operations. Used by Terraform integration tests on ephemeral KeyVaults."
 }
 
-module "infra_cd_common_key_vault_certificates" {
+module "automation_cd_common_key_vault_certificates" {
   source  = "pagopa-dx/azure-role-assignments/azurerm"
   version = "~> 2.1"
 
-  principal_id    = data.azurerm_user_assigned_identity.infra_cd.principal_id
+  principal_id    = data.azurerm_user_assigned_identity.automation_cd.principal_id
   subscription_id = data.azurerm_subscription.current.subscription_id
 
   key_vault = [
     {
       name                = module.azure_core_values.common_key_vault.name
       resource_group_name = module.azure_core_values.common_key_vault.resource_group_name
-      description         = "Allow Infra CD Identity to import the staging certificate used by the renew TLS certificate E2E test."
+      description         = "Allow Automation CD Tests Identity to import the staging certificate used by the renew TLS certificate E2E test."
       roles = {
         certificates = "writer"
       }
