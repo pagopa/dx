d1275dd957644feffacdbd6e43bfb48786d2b73a
Luca Cavallaro <luca.cavallaro@pagopa.it>
2026-08-28 11:24:30 +0200

    Update nx-docker-plugin dist/

--- packages/nx-dx-docker-plugin/dist/executors/docker-build/docker-build.js
+++ packages/nx-dx-docker-plugin/dist/executors/docker-build/docker-build.js
@@ -1,4 +1,4 @@
-const require_docker_run = require('../../docker-run-DXXchtdL.js');
+const require_docker_run = require('../../docker-run-CJXwapPp.js');
 
 //#region src/executors/docker-build/schema.ts
 const dockerBuildExecutorSchema = require_docker_run.dockerRunOptionsSchema;
