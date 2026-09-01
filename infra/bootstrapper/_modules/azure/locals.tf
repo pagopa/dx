b3cf8c275562259664969c263b7c285d9766e473
Krusty93 <andrea.grillo@pagopa.it>
2026-09-01 12:05:17 +0200

    remove dev resources

--- infra/bootstrapper/_modules/azure/locals.tf
+++ infra/bootstrapper/_modules/azure/locals.tf
@@ -1,14 +1,6 @@
 locals {
   project = "${var.environment.prefix}-${var.environment.env_short}-${local.location_short}"
 
-  naming_config = {
-    prefix          = var.environment.prefix,
-    environment     = var.environment.env_short,
-    domain          = var.environment.domain,
-    location        = var.environment.location,
-    instance_number = tonumber(var.environment.instance_number),
-  }
-
   location_short = {
     italynorth = "itn"
     westeurope = "weu"
@@ -30,6 +22,5 @@ locals {
     name                = replace("${local.project}tfstatest01", "-", "")
     resource_group_name = "${local.project}-tfstate-rg-01"
   }
-  tf_storage_account_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.tf_storage_account.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${local.tf_storage_account.name}"
 }
 
