190dfdc6f9998c8ee115637d9fc076e2aa35b730
Mario Mupo <mario.mupo@pagopa.it>
2026-09-02 09:46:10 +0200

    fix: minor, update cidrs for vpc and vnet

--- infra/core/uat/locals.tf
+++ infra/core/uat/locals.tf
@@ -16,11 +16,11 @@ locals {
   }
 
   aws = {
-    vpc_cidr = "10.0.0.0/16"
+    vpc_cidr = "10.5.0.0/16"
   }
 
   azure = {
-    vnet_cidr = "10.51.0.0/16"
+    vnet_cidr = "10.53.0.0/16"
   }
 
   tags = {
