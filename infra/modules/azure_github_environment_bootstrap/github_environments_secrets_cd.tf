101719195c8fc17cf6e1f7c5cd70fbd2451ce679
Andrea Grillo <andrea.grillo@pagopa.it>
2026-09-02 09:43:36 +0200

    Replace deprecated properties and add changelog (#2114)

--- infra/modules/azure_github_environment_bootstrap/github_environments_secrets_cd.tf
+++ infra/modules/azure_github_environment_bootstrap/github_environments_secrets_cd.tf
@@ -2,38 +2,38 @@
 resource "github_actions_environment_secret" "infra_cd" {
   for_each = local.infra_cd.secrets
 
-  repository      = var.repository.name
-  environment     = "infra-${local.env_name}-cd"
-  secret_name     = each.key
-  plaintext_value = each.value
+  repository  = var.repository.name
+  environment = "infra-${local.env_name}-cd"
+  secret_name = each.key
+  value       = each.value
 }
 
 #trivy:ignore:AVD-GIT-0002
 resource "github_actions_environment_secret" "automation_cd" {
   for_each = local.automation_cd.secrets
 
-  repository      = var.repository.name
-  environment     = "automation-${local.env_name}-cd"
-  secret_name     = each.key
-  plaintext_value = each.value
+  repository  = var.repository.name
+  environment = "automation-${local.env_name}-cd"
+  secret_name = each.key
+  value       = each.value
 }
 
 #trivy:ignore:AVD-GIT-0002
 resource "github_actions_environment_secret" "app_cd" {
   for_each = local.app_cd.secrets
 
-  repository      = var.repository.name
-  environment     = "app-${local.env_name}-cd"
-  secret_name     = each.key
-  plaintext_value = each.value
+  repository  = var.repository.name
+  environment = "app-${local.env_name}-cd"
+  secret_name = each.key
+  value       = each.value
 }
 
 #trivy:ignore:AVD-GIT-0002
 resource "github_actions_environment_secret" "opex_cd" {
   for_each = local.opex_cd.secrets
 
-  repository      = var.repository.name
-  environment     = "opex-${local.env_name}-cd"
-  secret_name     = each.key
-  plaintext_value = each.value
+  repository  = var.repository.name
+  environment = "opex-${local.env_name}-cd"
+  secret_name = each.key
+  value       = each.value
 }
