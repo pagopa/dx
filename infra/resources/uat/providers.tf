bda410fac85c146a23454b10263ad6d7371887c4
Mario Mupo <mario.mupo@pagopa.it>
2026-09-03 17:06:26 +0200

    fix: remove unused aliases

--- infra/resources/uat/providers.tf
+++ infra/resources/uat/providers.tf
@@ -64,20 +64,5 @@ provider "aws" {
   region = "eu-south-1"
 }
 
-provider "aws" {
-  alias  = "eu-central-1"
-  region = "eu-central-1"
-}
-
-provider "awscc" {
-  alias  = "eu-central-1"
-  region = "eu-central-1"
-}
-
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
-
 provider "awsdx" {}
 
